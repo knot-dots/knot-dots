616dc0966765155f3a5417af49e2dd8ab7bb1e36
Keegan Rankin <keegan@agaric.coop>
2023-04-10 12:34:00 -0700

    Configure screen size breakpoints for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ const config = {
 		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
 	],
 	theme: {
-		extend: {}
+		extend: {
+			// see default breakpoints at https://tailwindcss.com/docs/screens
+			screens: {
+				xs: '360px',
+				s: '768px',
+				m: '1440px',
+				l: '1920px'
+			}
+		}
 	},
 	plugins: [flowbitePlugin],
 	darkMode: 'class'
