7e6a0da97368fd6d78c50fc22e8ce6e039a29cbc
Keegan Rankin <keegan@agaric.coop>
2023-04-12 11:47:10 -0700

    Add a sticky navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,40 @@
 <script>
-  import "../app.css";
+	import '../app.css';
+	import {
+		ButtonGroup,
+		Button,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger
+	} from 'flowbite-svelte';
+	import logo from '$lib/assets/logo.png';
 </script>
 
-<slot />
+<div class="relative px-8">
+	<Navbar
+		navClass="px-2 s:px-4 py-2.5 absolute w-full z-20 top-0 left-0 border-b"
+		let:hidden
+		let:toggle
+	>
+		<NavBrand href="/">
+			<img src={logo} class="mr-3 h-6 s:h-9" alt="Knot Dots logo" />
+		</NavBrand>
+		<NavHamburger on:click={toggle} />
+		<ButtonGroup>
+			<Button>Strategies</Button>
+			<Button>Objectives</Button>
+			<Button>Measures</Button>
+		</ButtonGroup>
+
+		<NavUl {hidden}>
+			<!-- <NavLi href="/" active={true}>Home</NavLi> -->
+			<NavLi href="/about"><Button color="alternative" size="sm">Log in</Button></NavLi>
+			<NavLi href="/services"><Button size="sm">Register</Button></NavLi>
+		</NavUl>
+	</Navbar>
+	<div style="height:300px;" class="overflow-scroll pb-16">
+		<slot />
+	</div>
+</div>
