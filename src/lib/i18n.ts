e233f77bd5f5fc217a6acb5d58e1687e792a3a86
Stefan Freudenberg <stefan@agaric.com>
2023-04-15 22:18:15 +0200

    Add translations for German and English

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,10 @@
 import { browser } from '$app/environment';
-import { init } from 'svelte-i18n';
+import { init, register } from 'svelte-i18n';
 
 const defaultLocale = 'de';
+
+register('de', () => import('./locales/de.json'));
+register('en', () => import('./locales/en.json'));
 
 init({
 	fallbackLocale: defaultLocale,
