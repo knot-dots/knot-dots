51642628ffafc358a3d34338ebaddaf3e5965034
Stefan Freudenberg <stefan@agaric.com>
2023-04-16 13:28:28 +0200

    Add tests for the page layout

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,17 @@
+import { devices } from '@playwright/test';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'iphone8',
+			use: { ...devices['iPhone 8'] }
+		}
+	],
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,66 @@
 import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
+test('home screen has expected regions', async ({ page, viewport }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('navigation')).toBeVisible();
+	await expect(page.getByRole('main')).toBeVisible();
+
+	if (viewport && viewport.width >= 768) {
+		await expect(page.getByRole('complementary')).toBeVisible();
+	} else {
+		await expect(page.getByRole('complementary')).toBeHidden();
+	}
+});
+
+test('navigation contains expected elements', async ({ page, viewport }) => {
+	await page.goto('/');
+	await expect(
+		page.getByRole('navigation').getByRole('link', { name: 'Home screen' })
+	).toBeVisible();
+	await expect(
+		page.getByRole('navigation').getByRole('button', { name: 'Strategies' })
+	).toBeVisible();
+	await expect(
+		page.getByRole('navigation').getByRole('button', { name: 'Objectives' })
+	).toBeVisible();
+	await expect(
+		page.getByRole('navigation').getByRole('button', { name: 'Measures' })
+	).toBeVisible();
+
+	if (viewport && viewport.width >= 1440) {
+		await expect(page.getByRole('navigation').getByRole('link', { name: 'Log in' })).toBeVisible();
+		await expect(
+			page.getByRole('navigation').getByRole('link', { name: 'Register' })
+		).toBeVisible();
+	}
+
+	if (viewport && viewport.width >= 768) {
+		await expect(
+			page.getByRole('navigation').getByRole('button', { name: 'Open sidebar' })
+		).toBeHidden();
+	} else {
+		await expect(
+			page.getByRole('navigation').getByRole('button', { name: 'Open sidebar' })
+		).toBeVisible();
+	}
+});
+
+test('sidebar can be opened and closed', async ({ page, viewport }) => {
+	test.skip(!viewport || viewport?.width >= 768);
+
+	await page.goto('/');
+	await page.getByRole('button', { name: 'Open sidebar' }).click();
+	await expect(page.getByRole('complementary')).toBeVisible();
+	await page.getByRole('button', { name: 'Close sidebar' }).click();
+	await expect(page.getByRole('complementary')).toBeHidden();
+});
+
+test('sidebar can be collapsed and expanded', async ({ page, viewport }) => {
+	test.skip(!viewport || viewport?.width < 768);
+
+	await page.goto('/');
+	await expect(page.getByRole('button', { name: 'Boards' })).toBeVisible();
+	await page.getByRole('button', { name: 'Collapse sidebar' }).click();
+	await expect(page.getByRole('button', { name: 'Boards' })).toBeHidden();
+	await page.getByRole('button', { name: 'Expand sidebar' }).click();
+	await expect(page.getByRole('button', { name: 'Boards' })).toBeVisible();
 });
