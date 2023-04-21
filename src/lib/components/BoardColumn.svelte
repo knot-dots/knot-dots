496ea98e84aa6dc284212d642d696f00def92de2
Keegan Rankin <keegan@agaric.coop>
2023-04-21 09:58:04 -0700

    Create the goals board with columns and cards

---
+++ src/lib/components/BoardColumn.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import FilterIcon from '$lib/icons/FilterIcon.svelte';
+	import { _ } from 'svelte-i18n';
+	export let title: string;
+</script>
+
+<section>
+	<header>
+		<h2>{title}</h2>
+		<FilterIcon class="icon-20" />
+	</header>
+	<slot />
+	<footer>
+		<button class="primary" disabled>{$_('add_item')}</button>
+	</footer>
+</section>
+
+<style>
+	section {
+		flex-basis: 320px;
+		flex-grow: 0;
+		flex-shrink: 0;
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+	}
+
+	footer,
+	footer button {
+		width: 100%;
+	}
+
+	header {
+		margin-bottom: 24px;
+		height: 59px;
+		background: white;
+		border-radius: 8px;
+		box-shadow: var(--shadow-md);
+		padding: 16px;
+		color: var(--color-gray-800);
+		display: flex;
+		justify-content: space-between;
+	}
+
+	header h2 {
+		font-size: inherit;
+		font-weight: 600;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="mb-8">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Board from '$lib/components/Board.svelte';
+</script>
+
+<Board />
