a9f0b80b4b1184c8a4ef46a8af68ff411ce52d6b
Stefan Freudenberg <stefan@agaric.com>
2023-04-23 22:37:38 +0200

    Replace type declaration with satisfies statement

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,11 @@ import type { Handle } from '@sveltejs/kit';
 import { locale } from 'svelte-i18n';
 import { getPool } from '$lib/server/db';
 
-export const handle: Handle = async ({ event, resolve }) => {
+export const handle = (async ({ event, resolve }) => {
 	const lang = event.request.headers.get('accept-language')?.split(',')[0];
 	if (lang) {
 		locale.set(lang);
 	}
 	event.locals.pool = await getPool();
 	return resolve(event);
-};
+}) satisfies Handle;
