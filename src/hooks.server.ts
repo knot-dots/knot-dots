f236bf125ff1c2dc6a15a7d00be2001bc6c90b80
Stefan Freudenberg <stefan@agaric.com>
2023-05-10 15:29:22 +0200

    Move JWT verification to server hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
 import type { Handle, HandleServerError } from '@sveltejs/kit';
+import * as jose from 'jose';
 import { Roarr as log } from 'roarr';
 import { serializeError } from 'serialize-error';
 import { _, locale, unwrapFunctionStore } from 'svelte-i18n';
+import { env as privateEnv } from '$env/dynamic/private';
+import { env } from '$env/dynamic/public';
 import { getPool } from '$lib/server/db';
 
 export const handle = (async ({ event, resolve }) => {
@@ -9,7 +12,25 @@ export const handle = (async ({ event, resolve }) => {
 	if (lang) {
 		locale.set(lang);
 	}
+
+	if (event.request.headers.get('Authorization')?.startsWith('Bearer ')) {
+		const token = (event.request.headers.get('Authorization') as string).substring(7);
+		const jwks = jose.createRemoteJWKSet(
+			new URL(`${privateEnv.KC_URL}/realms/${env.PUBLIC_KC_REALM}/protocol/openid-connect/certs`)
+		);
+		try {
+			const { payload } = await jose.jwtVerify(token, jwks, {
+				issuer: `${env.PUBLIC_KC_URL}/realms/${env.PUBLIC_KC_REALM}`,
+				requiredClaims: ['iss', 'sub']
+			});
+			event.locals.user = { issuer: payload.iss ?? '', subject: payload.sub ?? '' };
+		} catch (error) {
+			log.warn(serializeError(error), String(error));
+		}
+	}
+
 	event.locals.pool = await getPool();
+
 	return resolve(event);
 }) satisfies Handle;
 
--- src/routes/container/[type]/[guid]/+page.server.ts
+++ src/routes/container/[type]/[guid]/+page.server.ts
@@ -1,6 +1,4 @@
 import { error, redirect } from '@sveltejs/kit';
-import * as jose from 'jose';
-import { env as privateEnv } from '$env/dynamic/private';
 import { env } from '$env/dynamic/public';
 import { isContainerType } from '$lib/models';
 import type { SustainableDevelopmentGoal } from '$lib/models';
@@ -18,36 +16,9 @@ export const actions = {
 			throw error(404, 'Unknown container type');
 		}
 
-		if (
-			!request.headers.has('Authorization') ||
-			!request.headers.get('Authorization')?.startsWith('Bearer ')
-		) {
+		if (locals.user == null) {
 			throw error(401, { message: 'Missing authorization' });
 		}
-
-		const token = (request.headers.get('Authorization') as string).substring(7);
-
-		const jwks = jose.createRemoteJWKSet(
-			new URL(`${privateEnv.KC_URL}/realms/${env.PUBLIC_KC_REALM}/protocol/openid-connect/certs`)
-		);
-		const {
-			payload: { iss, sub }
-		} = await jose
-			.jwtVerify(token, jwks, {
-				issuer: `${env.PUBLIC_KC_URL}/realms/${env.PUBLIC_KC_REALM}`,
-				requiredClaims: ['iss', 'sub']
-			})
-			.catch((e) => {
-				if (e instanceof jose.errors.JWTInvalid) {
-					throw error(401, { message: 'Invalid token' });
-				} else if (e instanceof jose.errors.JWTExpired) {
-					throw error(401, { message: 'Expired token' });
-				} else if (e instanceof jose.errors.JWTClaimValidationFailed) {
-					throw error(401, { message: 'Insufficient claims' });
-				} else {
-					throw e;
-				}
-			});
 
 		const data = await request.formData();
 		const payload = {
@@ -59,12 +30,6 @@ export const actions = {
 		const relation = data
 			.getAll('is-part-of')
 			.map((v) => ({ predicate: 'is-part-of', object: Number(v) }));
-		const user = [
-			{
-				issuer: iss as string,
-				subject: sub as string
-			}
-		];
 		const result = await locals.pool.connect(
 			updateContainer({
 				guid: params.guid,
@@ -72,7 +37,7 @@ export const actions = {
 				type: params.type,
 				realm: env.PUBLIC_KC_REALM ?? '',
 				relation,
-				user
+				user: [locals.user]
 			})
 		);
 
