094f6e3b873c62e46afa2e7c0b6adbbd9c2f2ca1
Keegan Rankin <keegan@agaric.coop>
2023-05-26 11:18:13 -0700

    Introduce class to distinguish layouts for boards and details pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 	import { onMount, setContext } from 'svelte';
 	import { _ } from 'svelte-i18n';
 	import '../app.css';
+	import { page } from '$app/stores';
 	import Navigation from '$lib/components/Navigation.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
 	import { initKeycloak, key } from '$lib/authentication';
 	import type { KeycloakContext } from '$lib/authentication';
-
 	let kc: Keycloak;
 
 	setContext<KeycloakContext>(key, {
@@ -21,6 +21,10 @@
 			silentCheckSsoFallback: false
 		});
 	});
+
+	function isBoardLayout() {
+		return ['/', '/measures'].includes($page.url.pathname);
+	}
 </script>
 
 <svelte:head>
@@ -30,7 +34,7 @@
 <Navigation />
 <div>
 	<Sidebar />
-	<main>
+	<main class:board-layout={isBoardLayout()}>
 		<slot />
 	</main>
 </div>
@@ -48,6 +52,9 @@
 		background-color: var(--color-gray-100);
 		min-width: 0;
 		padding: 1rem 1rem 0;
+	}
+
+	main.board-layout {
 		display: flex;
 	}
 </style>
