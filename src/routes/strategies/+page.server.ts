6fee050903b4032d93d74f35af72f4ea66e12db2
Keegan Rankin <keegan@agaric.coop>
2023-06-13 17:56:46 -0700

    Add page and server templates for strategies board
    First draft based on measures

---
+++ src/routes/strategies/+page.server.ts
@@ -0,0 +1,38 @@
+import {
+	getAllRelatedContainers,
+	getAllDirectlyRelatedContainers,
+	getContainerByGuid,
+	getManyContainersByType,
+	maybePartOf
+} from '$lib/server/db';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ locals, url }) => {
+	let containers;
+	let containerPreviewData;
+	let isPartOfOptions;
+	let relatedContainers;
+	if (url.searchParams.has('related-to')) {
+		containers = await locals.pool.connect(
+			getAllRelatedContainers(url.searchParams.get('related-to') as string)
+		);
+	} else {
+		containers = await locals.pool.connect(
+			getManyContainersByType(
+				'strategy',
+				url.searchParams.getAll('category'),
+				url.searchParams.get('terms') ?? '',
+				url.searchParams.get('sort') ?? ''
+			)
+		);
+	}
+	if (url.searchParams.has('container-preview')) {
+		const previewGuid = url.searchParams.get('container-preview') ?? '';
+		containerPreviewData = await locals.pool.connect(getContainerByGuid(previewGuid));
+		[isPartOfOptions, relatedContainers] = await Promise.all([
+			locals.pool.connect(maybePartOf(containerPreviewData.type)),
+			locals.pool.connect(getAllDirectlyRelatedContainers(containerPreviewData))
+		]);
+	}
+	return { containers, containerPreviewData, isPartOfOptions, relatedContainers };
+}) satisfies PageServerLoad;
---
+++ src/routes/strategies/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import Board from '$lib/components/Board.svelte';
+	import BoardColumn from '$lib/components/BoardColumn.svelte';
+	import Card from '$lib/components/Card.svelte';
+	import Overlay from '$lib/components/Overlay.svelte';
+	import { levels } from '$lib/models';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<Board>
+	{#each levels.options as levelOption}
+		<BoardColumn containerType="strategy" title={$_(levelOption)}>
+			{#each data.containers.filter((c) => 'level' in c.payload && c.payload.level == levelOption) as container}
+				<Card
+					guid={container.guid}
+					title={container.payload.title}
+					summary={container.payload.summary ?? ''}
+					category={$_(container.payload.category)}
+				/>
+			{/each}
+		</BoardColumn>
+	{/each}
+</Board>
+
+{#if data.containerPreviewData}
+	<Overlay
+		containerPreviewData={data.containerPreviewData}
+		isPartOfOptions={data.isPartOfOptions ?? []}
+		relatedContainers={data.relatedContainers ?? []}
+	/>
+{/if}
