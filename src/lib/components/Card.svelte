6ca4bb7b67a9f4074a355da015d648228a7712ed
Stefan Freudenberg <stefan@agaric.com>
2023-06-24 16:28:21 +0200

    Add status badges to measure cards

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { Icon, Share } from 'svelte-hero-icons';
+	import { Icon, LightBulb, Share } from 'svelte-hero-icons';
 	import { _ } from 'svelte-i18n';
 	import { page } from '$app/stores';
 	import ProgressBar from '$lib/components/ProgressBar.svelte';
 	import type { Container } from '$lib/models';
 	import { filtersToggle, sidebarToggle, sortToggle } from '$lib/stores';
+	import { statusColors, statusIcons } from '$lib/models';
 
 	export let container: Container;
 
@@ -64,6 +65,13 @@
 					contributors={relatedContainers}
 					compact
 				/>
+			{:else if 'status' in container.payload}
+				<div class="badges">
+					<span class="badge badge--{statusColors.get(container.payload.status)}">
+						<Icon src={statusIcons.get(container.payload.status) ?? LightBulb} size="16" mini />
+						{$_(container.payload.status)}
+					</span>
+				</div>
 			{:else}
 				<div class="badges">
 					<span class="badge">{$_(container.payload.category)}</span>
@@ -125,9 +133,37 @@
 	}
 
 	.badge {
-		text-align: center;
-		padding: 3px 10px;
-		background: var(--color-red-300);
+		--bg-color: var(--color-indigo-050);
+		--color: var(--color-indigo-500);
+
+		align-items: center;
+		background-color: var(--bg-color);
 		border-radius: 6px;
+		color: var(--color);
+		display: inline-flex;
+		justify-content: center;
+		gap: 0.25rem;
+		padding: 0.125rem 0.75rem;
+		text-align: center;
+	}
+
+	.badge.badge--red {
+		--bg-color: var(--color-red-050);
+		--color: var(--color-red-600);
+	}
+
+	.badge.badge--orange {
+		--bg-color: var(--color-orange-050);
+		--color: var(--color-orange-600);
+	}
+
+	.badge.badge--yellow {
+		--bg-color: var(--color-yellow-050);
+		--color: var(--color-yellow-400);
+	}
+
+	.badge.badge--green {
+		--bg-color: var(--color-green-050);
+		--color: var(--color-green-500);
 	}
 </style>
