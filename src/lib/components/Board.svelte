133198b7e7fdd41afe8f37b8f9b812a45b598bc4
Stefan Freudenberg <stefan@agaric.com>
2023-06-30 18:04:59 +0200

    Fix board height calculation not adjusting to margins

--- src/lib/components/Board.svelte
+++ src/lib/components/Board.svelte
@@ -4,11 +4,13 @@
 
 <style>
 	.board {
+		--margin-y: 0.75rem;
+
 		display: flex;
 		flex: 1 1;
-		gap: 0.75rem;
-		height: calc(100% - 2rem);
-		margin: 0.75rem 0.375rem;
+		gap: var(--margin-y);
+		height: calc(100% - 2 * var(--margin-y));
+		margin: var(--margin-y) 0.375rem;
 		overflow: scroll;
 	}
 </style>
