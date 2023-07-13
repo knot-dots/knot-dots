dcc2c93e7d31fdb966ee52da7e4fa7fb2baa1cce
Stefan Freudenberg <stefan@agaric.com>
2023-07-13 12:58:02 +0200

    Move SDG icons to models

--- src/lib/components/ContainerDetailView.svelte
+++ src/lib/components/ContainerDetailView.svelte
@@ -2,49 +2,12 @@
 	import { Viewer } from 'bytemd';
 	import { _ } from 'svelte-i18n';
 	import { page } from '$app/stores';
-	import sdg01 from '$lib/assets/sdg/sdg-01.svg';
-	import sdg02 from '$lib/assets/sdg/sdg-02.svg';
-	import sdg03 from '$lib/assets/sdg/sdg-03.svg';
-	import sdg04 from '$lib/assets/sdg/sdg-04.svg';
-	import sdg05 from '$lib/assets/sdg/sdg-05.svg';
-	import sdg06 from '$lib/assets/sdg/sdg-06.svg';
-	import sdg07 from '$lib/assets/sdg/sdg-07.svg';
-	import sdg08 from '$lib/assets/sdg/sdg-08.svg';
-	import sdg09 from '$lib/assets/sdg/sdg-09.svg';
-	import sdg10 from '$lib/assets/sdg/sdg-10.svg';
-	import sdg11 from '$lib/assets/sdg/sdg-11.svg';
-	import sdg12 from '$lib/assets/sdg/sdg-12.svg';
-	import sdg13 from '$lib/assets/sdg/sdg-13.svg';
-	import sdg14 from '$lib/assets/sdg/sdg-14.svg';
-	import sdg15 from '$lib/assets/sdg/sdg-15.svg';
-	import sdg16 from '$lib/assets/sdg/sdg-16.svg';
-	import sdg17 from '$lib/assets/sdg/sdg-17.svg';
 	import ProgressBar from '$lib/components/ProgressBar.svelte';
-	import type { Container, SustainableDevelopmentGoal } from '$lib/models';
-	import { sustainableDevelopmentGoals } from '$lib/models';
+	import { sdgIcons } from '$lib/models';
+	import type { Container } from '$lib/models';
 
 	export let container: Container;
 	export let relatedContainers: Container[];
-
-	let sdgIcons = new Map<SustainableDevelopmentGoal, string>([
-		[sustainableDevelopmentGoals.enum['sdg.01'], sdg01],
-		[sustainableDevelopmentGoals.enum['sdg.02'], sdg02],
-		[sustainableDevelopmentGoals.enum['sdg.03'], sdg03],
-		[sustainableDevelopmentGoals.enum['sdg.04'], sdg04],
-		[sustainableDevelopmentGoals.enum['sdg.05'], sdg05],
-		[sustainableDevelopmentGoals.enum['sdg.06'], sdg06],
-		[sustainableDevelopmentGoals.enum['sdg.07'], sdg07],
-		[sustainableDevelopmentGoals.enum['sdg.08'], sdg08],
-		[sustainableDevelopmentGoals.enum['sdg.09'], sdg09],
-		[sustainableDevelopmentGoals.enum['sdg.10'], sdg10],
-		[sustainableDevelopmentGoals.enum['sdg.11'], sdg11],
-		[sustainableDevelopmentGoals.enum['sdg.12'], sdg12],
-		[sustainableDevelopmentGoals.enum['sdg.13'], sdg13],
-		[sustainableDevelopmentGoals.enum['sdg.14'], sdg14],
-		[sustainableDevelopmentGoals.enum['sdg.15'], sdg15],
-		[sustainableDevelopmentGoals.enum['sdg.16'], sdg16],
-		[sustainableDevelopmentGoals.enum['sdg.17'], sdg17]
-	]);
 
 	let isPage = $page.url.pathname == `/${container.payload.type}/${container.guid}`;
 
