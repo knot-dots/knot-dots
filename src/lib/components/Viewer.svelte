fe40f699fce17a4f00ea879ecb12a241042f1448
Stefan Freudenberg <stefan@agaric.com>
2023-07-30 09:16:46 +0200

    Add Viewer component

---
+++ src/lib/components/Viewer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { unified } from 'unified';
+	import remarkParse from 'remark-parse';
+	import remarkGfm from 'remark-gfm';
+	import remarkRehype from 'remark-rehype';
+	import rehypeStringify from 'rehype-stringify';
+
+	export let value = '';
+</script>
+
+{#await unified()
+	.use(remarkParse)
+	.use(remarkGfm)
+	.use(remarkRehype)
+	.use(rehypeStringify)
+	.process(value) then content}
+	<div class="markdown-body">
+		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		{@html content}
+	</div>
+{/await}
