511973b1ceef965fe5eaf3b9e6f71491e3630d13
Keegan Rankin <keegan@agaric.coop>
2023-08-08 13:22:31 -0700

    Add the tasks board

---
+++ src/routes/[type=containerType]/[guid=uuid]/tasks/+page.server.ts
@@ -0,0 +1,33 @@
+import {
+	getAllContainersRelatedToMeasure,
+	getAllContainerRevisionsByGuid,
+	getAllRelatedContainers,
+	maybePartOf,
+	getContainerByGuid
+} from '$lib/server/db';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ locals, params, url }) => {
+	const measure = await locals.pool.connect(getContainerByGuid(params.guid));
+	let containers
+	let overlayData;
+
+  containers = await locals.pool.connect(
+    getAllContainersRelatedToMeasure(measure.revision, { type: 'internal_objective.task' })
+  );
+	if (url.searchParams.has('container-preview')) {
+		const guid = url.searchParams.get('container-preview') ?? '';
+		const revisions = await locals.pool.connect(getAllContainerRevisionsByGuid(guid));
+		const container = revisions[revisions.length - 1];
+		const [isPartOfOptions, relatedContainers] = await Promise.all([
+			locals.pool.connect(maybePartOf(container.payload.type)),
+			locals.pool.connect(getAllRelatedContainers(guid, {}, ''))
+		]);
+		overlayData = {
+			isPartOfOptions,
+			relatedContainers,
+			revisions
+		};
+	}
+	return { containers, measure, overlayData };
+}) satisfies PageServerLoad;
---
+++ src/routes/[type=containerType]/[guid=uuid]/tasks/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import Board from '$lib/components/Board.svelte';
+	import BoardColumn from '$lib/components/BoardColumn.svelte';
+	import Card from '$lib/components/Card.svelte';
+	import Overlay from '$lib/components/Overlay.svelte';
+	import { isPartOf, isTaskContainer, payloadTypes } from '$lib/models';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const columns = [
+    {
+      title: 'task_status.idea',
+      payloadType: payloadTypes.enum['internal_objective.task']
+    },
+		{
+			title: 'task_status.in_planning',
+			payloadType: payloadTypes.enum['internal_objective.task']
+		},
+		{
+			title: 'task_status.in_progress',
+			payloadType: payloadTypes.enum['internal_objective.task']
+		},
+		{
+			title: 'task_status.done',
+			payloadType: payloadTypes.enum['internal_objective.task']
+		}
+	];
+</script>
+
+<Board>
+	{#each columns as column (column.title)}
+		<BoardColumn title={$_(column.title)} addItemUrl={`/${column.payloadType}/new/?is-part-of-measure=${data.measure.revision}`}>
+			{#each data.containers.filter((c) => isTaskContainer(c) && c.payload.taskStatus === column.title) as container}
+				<Card {container} relatedContainers={data.containers.filter(isPartOf)} />
+			{/each}
+		</BoardColumn>
+	{/each}
+</Board>
+
+{#if data.overlayData}
+	<Overlay {...data.overlayData} />
+{/if}
