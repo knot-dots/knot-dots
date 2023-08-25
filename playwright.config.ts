a841acebc3de74e0e4a6c5829a3b4c1d2561c99b
Stefan Freudenberg <stefan@agaric.com>
2023-08-25 17:37:36 +0200

    Give keycloak more time to get ready

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ const config: PlaywrightTestConfig = {
 		command: 'docker compose up --build preview',
 		port: 3000,
 		reuseExistingServer: !process.env.CI,
-		timeout: 120 * 1000
+		timeout: 180 * 1000
 	},
 	reporter: process.env.CI ? 'github' : 'list',
 	retries: process.env.CI ? 1 : 0,
