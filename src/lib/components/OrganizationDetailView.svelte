f6ca09520d9701b9e912041be5e97baac7397204
Stefan Freudenberg <stefan@agaric.com>
2023-08-25 10:15:37 +0200

    Enable overlay on organization pages

--- src/lib/components/OrganizationDetailView.svelte
+++ src/lib/components/OrganizationDetailView.svelte
@@ -14,74 +14,85 @@
 	let isPage = $page.url.pathname == `/${container.payload.type}/${container.guid}`;
 </script>
 
-<article class="details" class:details--page={isPage}>
-	<header>
-		<h2>
-			{container.payload.name}
-			<div class="icons">
-				{#if $user.isAuthenticated}
-					<a href="{container.guid}/edit" class="icons-element">
-						<Icon solid src={Pencil} size="20" />
-					</a>
-				{/if}
-				<button class="icons-element" type="button" on:click={() => window.history.back()}>
-					<Icon solid src={ChevronLeft} size="20" />
-				</button>
-			</div>
-		</h2>
-	</header>
+<div class="organization">
+	<article class="details" class:details--page={isPage}>
+		<header>
+			<h2>
+				{container.payload.name}
+				<div class="icons">
+					{#if $user.isAuthenticated}
+						<a href="{container.guid}/edit" class="icons-element">
+							<Icon solid src={Pencil} size="20" />
+						</a>
+					{/if}
+					<button class="icons-element" type="button" on:click={() => window.history.back()}>
+						<Icon solid src={ChevronLeft} size="20" />
+					</button>
+				</div>
+			</h2>
+		</header>
 
-	<div class="details-content">
-		<div class="details-content-column">
-			<slot name="data">
-				{#if 'description' in container.payload}
-					<div class="description">
-						<h3>{$_('description')}</h3>
-						<Viewer value={container.payload.description} />
-					</div>
-				{/if}
-				{#if 'image' in container.payload}
-					<div class="image">
-						<img alt={$_('cover_image')} src={container.payload.image} />
-					</div>
-				{/if}
-			</slot>
+		<div class="details-content">
+			<div class="details-content-column">
+				<slot name="data">
+					{#if 'description' in container.payload}
+						<div class="description">
+							<h3>{$_('description')}</h3>
+							<Viewer value={container.payload.description} />
+						</div>
+					{/if}
+					{#if 'image' in container.payload}
+						<div class="image">
+							<img alt={$_('cover_image')} src={container.payload.image} />
+						</div>
+					{/if}
+				</slot>
+			</div>
+
+			<div class="details-content-column">
+				<div class="strategies">
+					<h3>{$_('strategies')}</h3>
+					<ul class="carousel">
+						{#each strategies as strategy}
+							<li>
+								<Card container={strategy} />
+							</li>
+						{/each}
+					</ul>
+				</div>
+				<div class="measures">
+					<h3>{$_('measures')}</h3>
+					<ul class="carousel">
+						{#each measures as measure}
+							<li>
+								<Card container={measure} />
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</div>
 		</div>
 
-		<div class="details-content-column">
-			<div class="strategies">
-				<h3>{$_('strategies')}</h3>
-				<ul class="carousel">
-					{#each strategies as strategy}
-						<li>
-							<Card container={strategy} />
-						</li>
-					{/each}
-				</ul>
-			</div>
-			<div class="measures">
-				<h3>{$_('measures')}</h3>
-				<ul class="carousel">
-					{#each measures as measure}
-						<li>
-							<Card container={measure} />
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</div>
-	</div>
-
-	{#if !isPage}
-		<footer>
-			<a class="button primary" href="/{container.payload.type}/{container.guid}">
-				{$_('read_more')}
-			</a>
-		</footer>
-	{/if}
-</article>
+		{#if !isPage}
+			<footer>
+				<a class="button primary" href="/{container.payload.type}/{container.guid}">
+					{$_('read_more')}
+				</a>
+			</footer>
+		{/if}
+	</article>
+</div>
 
 <style>
+	.organization {
+		flex: 1 1;
+		overflow-x: auto;
+	}
+
+	article {
+		min-width: calc(100vw - 20rem);
+	}
+
 	@container (min-width: 768px) {
 		.details-content {
 			flex-direction: column;
