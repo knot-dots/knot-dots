d7ae369662474d5dd6f198a80366db91b846258c
Stefan Freudenberg <stefan@agaric.com>
2023-08-27 15:12:31 +0200

    Add type and predicate for organizational unit

--- src/lib/components/ContainerForm.svelte
+++ src/lib/components/ContainerForm.svelte
@@ -143,7 +143,7 @@
 	<header>
 		<label>
 			{$_(`${container.payload.type}`)}
-			{#if container.payload.type === payloadTypes.enum.organization}
+			{#if container.payload.type === payloadTypes.enum.organization || container.payload.type === payloadTypes.enum.organizational_unit}
 				<input name="name" type="text" bind:value={container.payload.name} required />
 			{:else}
 				<input name="title" type="text" bind:value={container.payload.title} required />
