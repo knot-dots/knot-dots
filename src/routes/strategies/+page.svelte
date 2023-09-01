284f1e596325410dfc3d30b5170afb9db6c75cf8
Stefan Freudenberg <stefan@agaric.com>
2023-09-01 20:54:32 +0200

    Remove column from strategies board

--- src/routes/strategies/+page.svelte
+++ src/routes/strategies/+page.svelte
@@ -11,12 +11,9 @@
 </script>
 
 <Board>
-	{#each levels.options as levelOption}
-		<BoardColumn
-			addItemUrl="/strategy/new?level={levelOption}"
-			title={$_(levelOption)}
-		>
-			{#each data.containers.filter((c) => 'level' in c.payload && c.payload.level == levelOption) as container}
+	{#each levels.options.filter((l) => l !== levels.enum['level.regional']) as levelOption}
+		<BoardColumn addItemUrl="/strategy/new?level={levelOption}" title={$_(levelOption)}>
+			{#each data.containers.filter((c) => 'level' in c.payload && c.payload.level === levelOption) as container}
 				<Card {container} />
 			{/each}
 		</BoardColumn>
