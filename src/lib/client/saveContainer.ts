e8126b0b48801db89774c05b78b050b5bb2157b0
Stefan Freudenberg <stefan@agaric.com>
2023-09-15 14:51:50 +0200

    Add function for saving containers

---
+++ src/lib/client/saveContainer.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+import { env } from '$env/dynamic/public';
+import { modifiedContainer, newContainer } from '$lib/models';
+import type { Container } from '$lib/models';
+import type Keycloak from 'keycloak-js';
+
+export default async function saveContainer(keycloak: Keycloak, container: Container) {
+	let url = '/container';
+	if (container.revision) {
+		url = `/container/${container.guid}/revision`;
+	}
+
+	const data = z.union([modifiedContainer, newContainer]).parse({
+		...container,
+		realm: env.PUBLIC_KC_REALM,
+		relation: container.relation
+			.filter((r) => ('guid' in container ? r.subject == container.revision : true))
+			.map(({ object, position, predicate }) => ({
+				predicate,
+				object,
+				position
+			})),
+		user: []
+	});
+
+	// Ensure a fresh token will be included in the Authorization header.
+	await keycloak.updateToken(-1).catch(() => null);
+
+	return await fetch(url, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			Authorization: `Bearer ${keycloak.token}`,
+			'Content-Type': 'application/json'
+		}
+	});
+}
