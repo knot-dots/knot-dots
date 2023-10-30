425e7b2632b0e2e3cd1770cf77b0c0b7c59fcf70
Stefan Freudenberg <stefan@agaric.com>
2023-10-30 17:09:56 +0100

    Apply recent chages to InternalObjectiveDetailView, too

--- src/lib/components/InternalObjectiveDetailView.svelte
+++ src/lib/components/InternalObjectiveDetailView.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
 	import { _, date } from 'svelte-i18n';
 	import { page } from '$app/stores';
 	import Progress from '$lib/components/Progress.svelte';
 	import Viewer from '$lib/components/Viewer.svelte';
 	import { isMeasureContainer, owners } from '$lib/models';
 	import type { AnyContainer, Container } from '$lib/models';
-	import { ability } from '$lib/stores';
 
 	export let container: Container;
 	export let relatedContainers: Container[];
 	export let revisions: AnyContainer[];
-
-	let mayShowRelationButton = getContext('mayShowRelationButton');
 
 	$: measure = isMeasureContainer(container)
 		? container
@@ -32,92 +28,69 @@
 </script>
 
 <article class="details">
-	<header>
-		<slot name="header" />
-	</header>
+	<slot name="data">
+		{#if 'summary' in container.payload}
+			<div class="summary">
+				<h3>{$_('summary')}</h3>
+				{container.payload.summary ?? ''}
+			</div>
+		{/if}
+		{#if 'description' in container.payload}
+			<div class="description">
+				<h3>{$_('description')}</h3>
+				<Viewer value={container.payload.description} />
+			</div>
+		{/if}
+		{#if 'progress' in container.payload}
+			<div class="progress">
+				<h3>{$_('progress')}</h3>
+				<Progress value={container.payload.progress} />
+			</div>
+		{/if}
+	</slot>
 
-	<div class="details-content">
-		<div class="details-content-column">
-			<slot name="data">
-				{#if 'summary' in container.payload}
-					<div class="summary">
-						<h3>{$_('summary')}</h3>
-						{container.payload.summary ?? ''}
-					</div>
-				{/if}
-				{#if 'description' in container.payload}
-					<div class="description">
-						<h3>{$_('description')}</h3>
-						<Viewer value={container.payload.description} />
-					</div>
-				{/if}
-				{#if 'progress' in container.payload}
-					<div class="progress">
-						<h3>{$_('progress')}</h3>
-						<Progress value={container.payload.progress} />
-					</div>
-				{/if}
-			</slot>
+	<slot name="meta">
+		<div class="meta">
+			<h3 class="meta-key">{$_('object')}</h3>
+			<p class="meta-value">{$_(container.payload.type)}</p>
 		</div>
-
-		<div class="details-content-column">
-			<slot name="meta">
-				<div class="meta">
-					<h3 class="meta-key">{$_('object')}</h3>
-					<p class="meta-value">{$_(container.payload.type)}</p>
-				</div>
-				{#if measure}
-					<div class="meta">
-						<h3 class="meta-key">{$_('measure')}</h3>
-						<p class="meta-value">
-							<a href={containerURL(measure.payload.type, measure.guid)}>
-								{$_(measure.payload.title)}
-							</a>
-						</p>
-					</div>
-				{/if}
-				<div class="meta">
-					<h3 class="meta-key">{$_('owned_by')}</h3>
-					<ul class="meta-value">
-						{#each owners( container, [...$page.data.organizations, ...$page.data.organizationalUnits] ) as owner}
-							<li>{owner.payload.name}</li>
-						{/each}
-					</ul>
-				</div>
-				{#if 'fulfillmentDate' in container.payload && container.payload.fulfillmentDate}
-					<div class="meta">
-						<h3 class="meta-key">{$_('fulfillment_date')}</h3>
-						<p class="meta-value">
-							{$date(new Date(container.payload.fulfillmentDate), { format: 'medium' })}
-						</p>
-					</div>
-				{/if}
-				<div class="meta">
-					<h3 class="meta-key">{$_('created_date')}</h3>
-					<ul class="meta-value">
-						<li>{$date(revisions[0].valid_from, { format: 'medium' })}</li>
-					</ul>
-				</div>
-				<div class="meta">
-					<h3 class="meta-key">{$_('modified_date')}</h3>
-					<ul class="meta-value">
-						<li>{$date(container.valid_from, { format: 'medium' })}</li>
-					</ul>
-				</div>
-			</slot>
+		{#if measure}
+			<div class="meta">
+				<h3 class="meta-key">{$_('measure')}</h3>
+				<p class="meta-value">
+					<a href={containerURL(measure.payload.type, measure.guid)}>
+						{$_(measure.payload.title)}
+					</a>
+				</p>
+			</div>
+		{/if}
+		<div class="meta">
+			<h3 class="meta-key">{$_('owned_by')}</h3>
+			<ul class="meta-value">
+				{#each owners( container, [...$page.data.organizations, ...$page.data.organizationalUnits] ) as owner}
+					<li>{owner.payload.name}</li>
+				{/each}
+			</ul>
 		</div>
-	</div>
-
-	{#if !isPage}
-		<footer>
-			<a class="button primary" href="/{container.payload.type}/{container.guid}">
-				{$_('read_more')}
-			</a>
-			{#if mayShowRelationButton && $ability.can('relate', container)}
-				<a class="button" href="?container-relations={container.guid}">
-					{$_('relations')}
-				</a>
-			{/if}
-		</footer>
-	{/if}
+		{#if 'fulfillmentDate' in container.payload && container.payload.fulfillmentDate}
+			<div class="meta">
+				<h3 class="meta-key">{$_('fulfillment_date')}</h3>
+				<p class="meta-value">
+					{$date(new Date(container.payload.fulfillmentDate), { format: 'medium' })}
+				</p>
+			</div>
+		{/if}
+		<div class="meta">
+			<h3 class="meta-key">{$_('created_date')}</h3>
+			<ul class="meta-value">
+				<li>{$date(revisions[0].valid_from, { format: 'medium' })}</li>
+			</ul>
+		</div>
+		<div class="meta">
+			<h3 class="meta-key">{$_('modified_date')}</h3>
+			<ul class="meta-value">
+				<li>{$date(container.valid_from, { format: 'medium' })}</li>
+			</ul>
+		</div>
+	</slot>
 </article>
