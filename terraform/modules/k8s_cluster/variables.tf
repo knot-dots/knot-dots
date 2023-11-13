d29ba97d5e859bcffc07bc57e7540cb597646ad7
Stefan Freudenberg <stefan@agaric.com>
2023-11-13 17:44:50 +0100

    Upgrade cluster to Kubernetes 1.28.2

--- terraform/modules/k8s_cluster/variables.tf
+++ terraform/modules/k8s_cluster/variables.tf
@@ -4,7 +4,7 @@ variable "cluster_name" {
 }
 
 variable "cluster_version" {
-  default     = "1.27.6"
+  default     = "1.28.2"
   description = "The version of Kubernetes the cluster is running."
   type        = string
 }
