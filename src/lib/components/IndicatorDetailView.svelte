3842bcc8c2a644dfb0e018eebf1f88a3af7d3e37
Stefan Freudenberg <stefan@agaric.com>
2023-11-28 11:22:52 +0100

    Remove unnecessary wrapper

--- src/lib/components/IndicatorDetailView.svelte
+++ src/lib/components/IndicatorDetailView.svelte
@@ -20,84 +20,82 @@
 
 	applicationState.update((state) => ({
 		...state,
-		containerDetailView: { activeTab: 'basic-data', tabs: ['basic-data'] }
+		containerDetailView: { tabs: [] }
 	}));
 </script>
 
 <article class="details">
-	{#if $applicationState.containerDetailView.activeTab === 'basic-data'}
-		<div class="details-tab" id="basic-data">
-			<div class="description">
-				<h3>{$_('description')}</h3>
-				<Viewer value={container.payload.description} />
-			</div>
-
-			<IndicatorChart {container} />
-
-			<div class="objectives">
-				<h3>{$_('objectives')}</h3>
-				<ul class="carousel">
-					{#each relatedContainers.filter((c) => isModelContainer(c) || isOperationalGoalContainer(c) || isStrategicGoalGoalContainer(c)) as objective}
-						<li>
-							<Card --height="100%" container={objective} />
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div class="meta">
-				<h3 class="meta-key">{$_('object')}</h3>
-				<p class="meta-value">{$_(container.payload.type)}</p>
-			</div>
-
-			<div class="meta">
-				<h3 class="meta-key">{$_('topic.label')}</h3>
-				<ul class="meta-value meta-value--topic">
-					{#each container.payload.topic as topic}
-						<li>{$_(topic)}</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div class="meta">
-				<h3 class="meta-key">{$_('category')}</h3>
-				<ul class="meta-value meta-value--category">
-					{#each container.payload.category as category}
-						<li>
-							<img
-								src={sdgIcons.get(category)}
-								alt={$_(category)}
-								title={$_(category)}
-								width="66"
-								height="66"
-							/>
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div class="meta">
-				<h3 class="meta-key">{$_('owned_by')}</h3>
-				<ul class="meta-value">
-					{#each owners( container, [...$page.data.organizations, ...$page.data.organizationalUnits] ) as owner}
-						<li>{owner.payload.name}</li>
-					{/each}
-				</ul>
-			</div>
-
-			<div class="meta">
-				<h3 class="meta-key">{$_('created_date')}</h3>
-				<ul class="meta-value">
-					<li>{$date(revisions[0].valid_from, { format: 'medium' })}</li>
-				</ul>
-			</div>
-
-			<div class="meta">
-				<h3 class="meta-key">{$_('modified_date')}</h3>
-				<ul class="meta-value">
-					<li>{$date(container.valid_from, { format: 'medium' })}</li>
-				</ul>
-			</div>
+	<div class="details-tab" id="basic-data">
+		<div class="description">
+			<h3>{$_('description')}</h3>
+			<Viewer value={container.payload.description} />
 		</div>
-	{/if}
+
+		<IndicatorChart {container} />
+
+		<div class="objectives">
+			<h3>{$_('objectives')}</h3>
+			<ul class="carousel">
+				{#each relatedContainers.filter((c) => isModelContainer(c) || isOperationalGoalContainer(c) || isStrategicGoalGoalContainer(c)) as objective}
+					<li>
+						<Card --height="100%" container={objective} />
+					</li>
+				{/each}
+			</ul>
+		</div>
+
+		<div class="meta">
+			<h3 class="meta-key">{$_('object')}</h3>
+			<p class="meta-value">{$_(container.payload.type)}</p>
+		</div>
+
+		<div class="meta">
+			<h3 class="meta-key">{$_('topic.label')}</h3>
+			<ul class="meta-value meta-value--topic">
+				{#each container.payload.topic as topic}
+					<li>{$_(topic)}</li>
+				{/each}
+			</ul>
+		</div>
+
+		<div class="meta">
+			<h3 class="meta-key">{$_('category')}</h3>
+			<ul class="meta-value meta-value--category">
+				{#each container.payload.category as category}
+					<li>
+						<img
+							src={sdgIcons.get(category)}
+							alt={$_(category)}
+							title={$_(category)}
+							width="66"
+							height="66"
+						/>
+					</li>
+				{/each}
+			</ul>
+		</div>
+
+		<div class="meta">
+			<h3 class="meta-key">{$_('owned_by')}</h3>
+			<ul class="meta-value">
+				{#each owners( container, [...$page.data.organizations, ...$page.data.organizationalUnits] ) as owner}
+					<li>{owner.payload.name}</li>
+				{/each}
+			</ul>
+		</div>
+
+		<div class="meta">
+			<h3 class="meta-key">{$_('created_date')}</h3>
+			<ul class="meta-value">
+				<li>{$date(revisions[0].valid_from, { format: 'medium' })}</li>
+			</ul>
+		</div>
+
+		<div class="meta">
+			<h3 class="meta-key">{$_('modified_date')}</h3>
+			<ul class="meta-value">
+				<li>{$date(container.valid_from, { format: 'medium' })}</li>
+			</ul>
+		</div>
+	</div>
 </article>
