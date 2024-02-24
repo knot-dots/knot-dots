9357f14dbb61b04e75db62eb80c118849eb90a19
Stefan Freudenberg <stefan@agaric.com>
2024-02-24 11:50:14 +0100

    Add custom icon collection

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vitest/config';
 
@@ -6,7 +7,19 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		Icons({
-			compiler: 'svelte'
+			compiler: 'svelte',
+			customCollections: {
+				knotdots: FileSystemIconLoader('./src/lib/assets/icons')
+			},
+			iconCustomizer(collection, icon, props) {
+				if (collection === 'heroicons') {
+					props.width = '20px';
+					props.height = '20px';
+				} else if (collection === 'knotdots') {
+					props.width = '20px';
+					props.height = '20px';
+				}
+			}
 		})
 	],
 	ssr: {
