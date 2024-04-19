6cc206ac75c04ec589d478cd85ae3892249afe62
Stefan Freudenberg <stefan@agaric.com>
2024-04-19 06:56:43 +0200

    Add IndicatorWokspaces component

---
+++ src/lib/components/IndicatorWorkspaces.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { _ } from 'svelte-i18n';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+</script>
+
+<li>
+	<label>
+		<input
+			type="radio"
+			checked={$page.url.pathname === '/indicators'}
+			on:click={async () => await goto('/indicators')}
+		/>
+		{$_('workspace.indicators')}
+	</label>
+</li>
+<li>
+	<label>
+		<input
+			type="radio"
+			checked={$page.url.pathname === '/indicator-monitoring'}
+			on:click={async () => await goto('/indicator-monitoring')}
+		/>
+		{$_('workspace.indicator_monitoring')}
+	</label>
+</li>
