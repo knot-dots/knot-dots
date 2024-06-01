ab0f28012f81a8bd545410a8313ecd02b9fad250
Stefan Freudenberg <stefan@agaric.com>
2024-06-01 12:23:06 +0200

    Add IndicatorTemplateForm component

---
+++ src/lib/components/IndicatorTemplateForm.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import Editor from '$lib/components/Editor.svelte';
+	import ListBox from '$lib/components/ListBox.svelte';
+	import {
+		audience,
+		indicatorCategories,
+		measureTypes,
+		sustainableDevelopmentGoals,
+		topics,
+		units
+	} from '$lib/models';
+	import type { ContainerFormTabKey, IndicatorTemplateContainer } from '$lib/models';
+	import { ability, applicationState } from '$lib/stores';
+
+	export let container: IndicatorTemplateContainer;
+
+	applicationState.update((state) => ({
+		...state,
+		containerForm: {
+			activeTab: 'guid' in container ? 'basic-data' : 'metadata',
+			tabs: [...('guid' in container ? [] : ['metadata' as ContainerFormTabKey]), 'basic-data']
+		}
+	}));
+</script>
+
+<fieldset class="form-tab" id="metadata">
+	<legend>{$_('form.metadata')}</legend>
+
+	<label>
+		{$_('label.unit')}
+		<select name="unit" bind:value={container.payload.unit}>
+			{#each units.options as unitOption}
+				<option value={unitOption}>{$_(unitOption)}</option>
+			{/each}
+		</select>
+	</label>
+
+	<ListBox
+		label={$_('audience')}
+		options={audience.options}
+		bind:value={container.payload.audience}
+	/>
+</fieldset>
+
+<fieldset class="form-tab" id="basic-data">
+	<legend>{$_('form.basic_data')}</legend>
+
+	{#key 'guid' in container ? container.guid : ''}
+		<Editor label={$_('description')} bind:value={container.payload.description} />
+
+		<Editor
+			label={$_('indicator.historical_values_intro')}
+			bind:value={container.payload.historicalValuesIntro}
+		/>
+
+		<Editor
+			label={$_('indicator.objectives_intro')}
+			bind:value={container.payload.objectivesIntro}
+		/>
+
+		<Editor label={$_('indicator.measures_intro')} bind:value={container.payload.measuresIntro} />
+	{/key}
+
+	{#if $ability.can('update', container, 'indicatorCategory')}
+		<ListBox
+			label={$_('indicator_category')}
+			options={indicatorCategories.options}
+			bind:value={container.payload.indicatorCategory}
+		/>
+	{/if}
+
+	<ListBox
+		label={$_('measure_type')}
+		options={measureTypes.options}
+		bind:value={container.payload.measureType}
+	/>
+
+	<ListBox
+		label={$_('topic.label')}
+		options={topics.options}
+		bind:value={container.payload.topic}
+	/>
+
+	<ListBox
+		label={$_('category')}
+		options={sustainableDevelopmentGoals.options}
+		bind:value={container.payload.category}
+	/>
+</fieldset>
