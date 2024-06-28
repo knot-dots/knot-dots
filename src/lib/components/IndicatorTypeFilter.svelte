2b8ccb7a9634011cb2cd858dcf7080510708b7f6
Stefan Freudenberg <stefan@agaric.com>
2024-06-28 13:47:42 +0200

    Remove initial value from filter

--- src/lib/components/IndicatorTypeFilter.svelte
+++ src/lib/components/IndicatorTypeFilter.svelte
@@ -2,12 +2,9 @@
 	import { _ } from 'svelte-i18n';
 	import Filter from '$lib/components/Filter.svelte';
 	import { indicatorTypes } from '$lib/models';
-
-	export let initialValue = [indicatorTypes.enum['indicator_type.key']];
 </script>
 
 <Filter
-	{initialValue}
 	key="indicatorType"
 	label={$_('indicator_type')}
 	options={indicatorTypes.options.map((o) => [$_(o), o])}
