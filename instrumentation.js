bb798a18819de168c88266220d7dbe72686a2d20
Stefan Freudenberg <stefan@agaric.com>
2024-07-07 14:46:10 +0200

    Disable metrics

--- instrumentation.js
+++ instrumentation.js
@@ -1,14 +1,9 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 
 const sdk = new NodeSDK({
 	traceExporter: new OTLPTraceExporter(),
-	metricReader: new PeriodicExportingMetricReader({
-		exporter: new OTLPMetricExporter()
-	}),
 	instrumentations: [getNodeAutoInstrumentations()]
 });
 
