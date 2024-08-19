8791d8b788e62522b45fbb47df72d4b1a732bf95
Stefan Freudenberg <stefan@agaric.com>
2024-08-19 12:20:17 +0200

    Ensure password is revalidated on change

--- src/lib/components/SignupDialog.svelte
+++ src/lib/components/SignupDialog.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { passwordStrength } from 'check-password-strength'
+	import { passwordStrength } from 'check-password-strength';
 	import { _ } from 'svelte-i18n';
 	import { page } from '$app/stores';
 	import Dialog from '$lib/components/Dialog.svelte';
@@ -16,28 +16,37 @@
 	let familyName = '';
 	let error = '';
 
-	async function handleSubmit(event: {currentTarget: HTMLFormElement}): Promise<void> {
+	async function checkPasswordsMatch(event: { currentTarget: HTMLInputElement }) {
 		if (password != passwordConfirm) {
 			passwordConfirmInput.setCustomValidity($_('signup_dialog.password_mismatch'));
 			event.currentTarget.reportValidity();
+		} else {
+			passwordConfirmInput.setCustomValidity('');
 		}
+	}
 
+	async function checkPasswordStrength(event: { currentTarget: HTMLInputElement }) {
 		if (passwordStrength(password).id == 0) {
 			passwordInput.setCustomValidity($_('signup_dialog.password_too_weak'));
+			event.currentTarget.reportValidity();
+		} else {
+			passwordInput.setCustomValidity('');
 		}
+	}
 
+	async function handleSubmit(event: { currentTarget: HTMLFormElement }): Promise<void> {
 		const response = await fetch(`/user/${$page.url.searchParams.get('signup')}/signup`, {
-			body: JSON.stringify({familyName, givenName, password}),
+			body: JSON.stringify({ familyName, givenName, password }),
 			headers: { 'Content-Type': 'application/json' },
-			method: 'POST',
-		})
+			method: 'POST'
+		});
 
 		if (response.ok) {
 			dialog.close();
 			const url = new URL(window.location.href);
 			url.search = '';
-			url.hash = overlayKey.enum.profile
-			await signIn('keycloak', {callbackUrl: url.toString()});
+			url.hash = overlayKey.enum.profile;
+			await signIn('keycloak', { callbackUrl: url.toString() });
 		} else {
 			error = $_('signup_dialog.unexpected_error');
 		}
@@ -52,22 +61,50 @@
 		<p>
 			{#if error}{$_('signup_dialog.unexpected_error')}{:else}{$_('signup_dialog.message')}{/if}
 		</p>
-		<input type="hidden" name="email" autocomplete="username" value={$page.data.user?.email}/>
+		<input type="hidden" name="email" autocomplete="username" value={$page.data.user?.email} />
 		<label>
 			{$_('signup_dialog.given_name')}
-			<input type="text" name="givenName" autocomplete="given-name" bind:value={givenName} required/>
+			<input
+				type="text"
+				name="givenName"
+				autocomplete="given-name"
+				bind:value={givenName}
+				required
+			/>
 		</label>
 		<label>
 			{$_('signup_dialog.family_name')}
-			<input type="text" name="familyName" autocomplete="family-name" bind:value={familyName} required/>
+			<input
+				type="text"
+				name="familyName"
+				autocomplete="family-name"
+				bind:value={familyName}
+				required
+			/>
 		</label>
 		<label>
 			{$_('signup_dialog.password')}
-			<input type="password" name="password" autocomplete="new-password" bind:this={passwordInput} bind:value={password} required/>
+			<input
+				type="password"
+				name="password"
+				autocomplete="new-password"
+				bind:this={passwordInput}
+				bind:value={password}
+				on:change={checkPasswordStrength}
+				required
+			/>
 		</label>
 		<label>
 			{$_('signup_dialog.password_confirm')}
-			<input type="password" name="passwordConfirm" autocomplete="new-password" bind:this={passwordConfirmInput} bind:value={passwordConfirm} required/>
+			<input
+				type="password"
+				name="passwordConfirm"
+				autocomplete="new-password"
+				bind:this={passwordConfirmInput}
+				bind:value={passwordConfirm}
+				on:change={checkPasswordsMatch}
+				required
+			/>
 		</label>
 		<button class="primary" type="submit">{$_('signup_dialog.submit')}</button>
 	</form>
