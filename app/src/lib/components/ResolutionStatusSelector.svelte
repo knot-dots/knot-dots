e71d24a62960395f6d1d356f2cadc65828ec498f
Stefan Freudenberg <stefan@agaric.com>
2024-10-21 16:49:51 +0200

    Fix layout of resolution status selector

--- app/src/lib/components/ResolutionStatusSelector.svelte
+++ app/src/lib/components/ResolutionStatusSelector.svelte
@@ -6,8 +6,13 @@
 	export let value: string | undefined;
 </script>
 
-<ListBox
-	label={$_('resolution_status')}
-	options={resolutionStatus.options.map((o) => ({ value: o, label: $_(o) }))}
-	bind:value
-/>
+<div class="meta">
+	<p class="meta-key">{$_('resolution_status')}</p>
+	<div class="meta-value">
+		<ListBox
+			label={$_('resolution_status')}
+			options={resolutionStatus.options.map((o) => ({ value: o, label: $_(o) }))}
+			bind:value
+		/>
+	</div>
+</div>
