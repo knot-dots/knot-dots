0dd9062ffc62bd79741bfcdebf3e834ea8847240
Stefan Freudenberg <stefan@agaric.com>
2024-11-29 11:20:11 +0100

    Add table display of chapters to strategy detail view

--- app/src/lib/components/EditableStrategyDetailView.svelte
+++ app/src/lib/components/EditableStrategyDetailView.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+	import { flip } from 'svelte/animate';
 	import { _ } from 'svelte-i18n';
+	import { dragHandleZone } from 'svelte-dnd-action';
 	import PlusSmall from '~icons/heroicons/plus-small-solid';
+	import { invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { env } from '$env/dynamic/public';
+	import debouncedSave from '$lib/client/debouncedSave';
+	import saveContainer from '$lib/client/saveContainer';
 	import EditableAudience from '$lib/components/EditableAudience.svelte';
 	import EditableCategory from '$lib/components/EditableCategory.svelte';
 	import EditableChapter from '$lib/components/EditableChapter.svelte';
@@ -10,6 +15,7 @@
 	import EditableContainerDetailView from '$lib/components/EditableContainerDetailView.svelte';
 	import EditableLevel from '$lib/components/EditableLevel.svelte';
 	import EditableOwnedBy from '$lib/components/EditableOwnedBy.svelte';
+	import EditableRow from '$lib/components/EditableRow.svelte';
 	import EditableStrategyType from '$lib/components/EditableStrategyType.svelte';
 	import EditableTopic from '$lib/components/EditableTopic.svelte';
 	import {
@@ -28,6 +34,36 @@
 	export let relatedContainers: Container[] = [];
 	export let revisions: AnyContainer[];
 
+	$: parts = relatedContainers
+		.filter(({ guid, relation }) =>
+			relation.some(
+				({ predicate }) =>
+					predicate === predicates.enum['is-part-of-strategy'] && guid != container.guid
+			)
+		)
+		.filter(({ payload }) => byPayloadType(payload.type, $page.url));
+
+	function handleDndConsider(event: CustomEvent<DndEvent<Container>>) {
+		parts = event.detail.items;
+	}
+
+	async function handleDndFinalize(event: CustomEvent<DndEvent<Container>>) {
+		parts = event.detail.items;
+		const relation = [
+			...parts.map(({ revision }, index) => ({
+				object: container.revision,
+				position: index,
+				predicate: predicates.enum['is-part-of-strategy'],
+				subject: revision
+			})),
+			...container.relation.filter(
+				({ predicate }) => predicate !== predicates.enum['is-part-of-strategy']
+			)
+		];
+		await saveContainer({ ...container, relation });
+		await invalidateAll();
+	}
+
 	function addChapterURL(url: URL, strategyRevision: number) {
 		const params = paramsFromFragment(url);
 		params.set('create', payloadTypes.enum.undefined);
@@ -44,66 +80,95 @@
 	}
 </script>
 
-<EditableContainerDetailView {container} {relatedContainers} {revisions} tabs={['basic-data']}>
-	<svelte:fragment slot="data">
-		<EditableLevel
-			editable={$applicationState.containerDetailView.editable}
-			bind:value={container.payload.level}
-		/>
+{#if $applicationState.containerDetailView.mode === 'view_mode.preview'}
+	<EditableContainerDetailView {container} {relatedContainers} {revisions} tabs={['basic-data']}>
+		<svelte:fragment slot="data">
+			<EditableLevel
+				editable={$applicationState.containerDetailView.editable}
+				bind:value={container.payload.level}
+			/>
 
-		<EditableStrategyType
-			editable={$applicationState.containerDetailView.editable}
-			bind:value={container.payload.strategyType}
-		/>
+			<EditableStrategyType
+				editable={$applicationState.containerDetailView.editable}
+				bind:value={container.payload.strategyType}
+			/>
 
-		<EditableChapterType
-			editable={$applicationState.containerDetailView.editable}
-			bind:value={container.payload.chapterType}
-		/>
+			<EditableChapterType
+				editable={$applicationState.containerDetailView.editable}
+				bind:value={container.payload.chapterType}
+			/>
 
-		<EditableTopic
-			editable={$applicationState.containerDetailView.editable}
-			bind:value={container.payload.topic}
-		/>
+			<EditableTopic
+				editable={$applicationState.containerDetailView.editable}
+				bind:value={container.payload.topic}
+			/>
 
-		<EditableCategory
-			editable={$applicationState.containerDetailView.editable}
-			bind:value={container.payload.category}
-		/>
+			<EditableCategory
+				editable={$applicationState.containerDetailView.editable}
+				bind:value={container.payload.category}
+			/>
 
-		<EditableAudience
-			editable={$applicationState.containerDetailView.editable}
-			bind:value={container.payload.audience}
-		/>
+			<EditableAudience
+				editable={$applicationState.containerDetailView.editable}
+				bind:value={container.payload.audience}
+			/>
 
-		<EditableOwnedBy editable={$applicationState.containerDetailView.editable} bind:container />
-	</svelte:fragment>
+			<EditableOwnedBy editable={$applicationState.containerDetailView.editable} bind:container />
+		</svelte:fragment>
 
-	<svelte:fragment slot="extra">
-		<div class="chapters">
-			{#each relatedContainers
-				.filter( ({ guid, relation }) => relation.some(({ predicate }) => predicate === predicates.enum['is-part-of-strategy'] && guid != container.guid) )
-				.filter(({ payload }) => byPayloadType(payload.type, $page.url)) as part}
-				<form class="chapter" on:submit|preventDefault={debouncedSave(part)} novalidate>
-					<EditableChapter
-						container={part}
-						editable={$applicationState.containerDetailView.editable}
-						headingTag="h3"
-						isPartOf={container}
-						{relatedContainers}
-					/>
+		<svelte:fragment slot="extra">
+			<div class="chapters">
+				{#each parts as part}
+					<form class="chapter" on:submit|preventDefault={debouncedSave(part)} novalidate>
+						<EditableChapter
+							container={part}
+							editable={$applicationState.containerDetailView.editable}
+							headingTag="h3"
+							isPartOf={container}
+							{relatedContainers}
+						/>
+					</form>
+				{:else}
+					{#if $ability.can('create', containerOfType(payloadTypes.enum.undefined, $page.data.currentOrganization.guid, $page.data.currentOrganizationalUnit?.guid ?? null, container.managed_by, env.PUBLIC_KC_REALM))}
+						<a class="button" href={addChapterURL($page.url, container.revision)}>
+							<PlusSmall />
+							{$_('chapter')}
+						</a>
+					{/if}
+				{/each}
+			</div>
+		</svelte:fragment>
+	</EditableContainerDetailView>
+{:else if $applicationState.containerDetailView.mode === 'view_mode.table'}
+	<div class="table">
+		<div class="table-head">
+			<div class="row">
+				<div class="cell"></div>
+				<div class="cell"></div>
+				<div class="cell">{$_('fulfillment_date')}</div>
+				<div class="cell">{$_('status')}</div>
+				<div class="cell">{$_('object')}</div>
+			</div>
+		</div>
+		<div
+			class="table-body"
+			use:dragHandleZone={{ items: parts, flipDurationMs: 100 }}
+			on:consider={handleDndConsider}
+			on:finalize={handleDndFinalize}
+		>
+			{#each parts as part (part.guid)}
+				<form
+					class="row"
+					animate:flip={{ duration: 100 }}
+					on:submit|preventDefault={debouncedSave(part)}
+					novalidate
+				>
+					<EditableRow container={part} editable={$applicationState.containerDetailView.editable} />
 				</form>
-			{:else}
-				{#if $ability.can('create', containerOfType(payloadTypes.enum.undefined, $page.data.currentOrganization.guid, $page.data.currentOrganizationalUnit?.guid ?? null, container.managed_by, env.PUBLIC_KC_REALM))}
-					<a class="button" href={addChapterURL($page.url, container.revision)}>
-						<PlusSmall />
-						{$_('chapter')}
-					</a>
-				{/if}
 			{/each}
 		</div>
-	</svelte:fragment>
-</EditableContainerDetailView>
+	</div>
+{/if}
 
 <style>
 	.chapters {
@@ -117,5 +182,23 @@
 		gap: 0.5rem;
 		margin-bottom: 1.5rem;
 		max-width: 50rem;
+	}
+
+	.table {
+		margin-left: calc(4rem - 1.25rem - 2rem);
+		width: fit-content;
+	}
+
+	.table-body {
+		color: inherit;
+	}
+
+	.table-head {
+		background-color: transparent;
+		position: sticky;
+	}
+
+	.row {
+		border: none;
 	}
 </style>
