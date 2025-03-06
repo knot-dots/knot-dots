a20ef1ce51d8075f8b008ecafaf177bc77d1859c
Stefan Freudenberg <stefan@agaric.com>
2025-03-06 11:28:46 +0100

    Ensure all relations are migrated
    Some container records lack the relation to a creator. Since the column
    is not needed yet, we skip it for now.

--- migrate/sql/20250203152636_alter_container_relation.up.sql
+++ migrate/sql/20250203152636_alter_container_relation.up.sql
@@ -17,23 +17,22 @@ CREATE INDEX container_relation_valid_currently ON container_relation (valid_cur
 CREATE INDEX container_relation_valid_from_idx ON container_relation (valid_from);
 CREATE UNIQUE INDEX container_relation_object_predicate_subject_key ON container_relation (object, predicate, subject) WHERE valid_currently;
 
-INSERT INTO container_relation (creator, object, object_revision, predicate, subject, subject_revision, valid_currently, valid_from)
-SELECT cu.subject AS creator, o.guid AS object, o.revision, cr.predicate, s.guid AS subject, s.revision, false, s.valid_from
+INSERT INTO container_relation (object, object_revision, predicate, subject, subject_revision, valid_currently, valid_from)
+SELECT o.guid AS object, o.revision, cr.predicate, s.guid AS subject, s.revision, false, s.valid_from
 FROM container_relation cr
          JOIN container s ON cr.subject_revision = s.revision
-         JOIN container_user cu ON s.revision = cu.object AND cu.predicate = 'is-creator-of'
          JOIN container o ON cr.object_revision = o.revision
 ORDER BY cr.subject, cr.object
 ON CONFLICT ON CONSTRAINT container_relation_object_rev_predicate_subject_rev_key DO UPDATE
-    SET creator = excluded.creator, object = excluded.object, subject = excluded.subject, valid_currently = false, valid_from = excluded.valid_from;
+    SET object = excluded.object, subject = excluded.subject, valid_currently = false, valid_from = excluded.valid_from;
 
 WITH r AS (
     SELECT max(valid_from) AS valid_from, subject, max(subject_revision) AS subject_revision, object, max(object_revision) AS object_revision, predicate
     FROM container_relation cr
     GROUP BY subject, object, predicate
 )
-INSERT INTO container_relation (creator, deleted, object, object_revision, predicate, subject, subject_revision, valid_currently, valid_from)
-SELECT cu.subject AS creator, final.* FROM (
+INSERT INTO container_relation (deleted, object, object_revision, predicate, subject, subject_revision, valid_currently, valid_from)
+SELECT final.* FROM (
     SELECT CASE WHEN max(revision) > subject_revision THEN true ELSE false END AS deleted, object, object_revision, current.predicate, guid, max(revision) AS subject_revision, true AS valid_currently, max(valid_from) AS valid_from
     FROM (
         SELECT c.guid, c.revision AS revision, r.subject_revision, r.object, r.object_revision, r.predicate, c.valid_from
@@ -49,7 +48,6 @@ SELECT cu.subject AS creator, final.* FROM (
     ) AS current
     GROUP BY guid, subject_revision, object, object_revision, current.predicate
 ) AS final
-JOIN container_user cu ON cu.object = final.subject_revision AND cu.predicate = 'is-creator-of'
 ON CONFLICT ON CONSTRAINT container_relation_object_rev_predicate_subject_rev_key DO UPDATE SET valid_currently = true;
 
 ALTER TABLE container_relation DROP CONSTRAINT container_relation_object_rev_predicate_subject_rev_key;
