d806f1428ae18c36724901132f0a2a99e4b66284
Stefan Freudenberg <stefan@agaric.com>
2025-03-06 15:35:00 +0100

    Instrument load functions

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -1,3 +1,4 @@
+import { trace, type Span } from '@opentelemetry/api';
 import { error } from '@sveltejs/kit';
 import { Roarr as log } from 'roarr';
 import { isErrorLike, serializeError } from 'serialize-error';
@@ -14,67 +15,72 @@ import {
 import { findUserById } from '$lib/server/keycloak';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ fetch, locals, url }) => {
-	let currentOrganization;
-	let currentOrganizationalUnit: OrganizationalUnitContainer | undefined;
-	let user: KeycloakUser | undefined = undefined;
+const tracer = trace.getTracer('app');
 
-	async function filterVisibleAsync<T extends AnyContainer>(promise: Promise<Array<T>>) {
-		const containers = await promise;
-		return filterVisible(containers, locals.user);
-	}
+export const load: LayoutServerLoad = async ({ fetch, locals, url }) =>
+	tracer.startActiveSpan('layout.load', async (span: Span) => {
+		let currentOrganization;
+		let currentOrganizationalUnit: OrganizationalUnitContainer | undefined;
+		let user: KeycloakUser | undefined = undefined;
 
-	const [organizations, organizationalUnits] = await Promise.all([
-		filterVisibleAsync(locals.pool.connect(getManyOrganizationContainers({}, 'alpha'))),
-		filterVisibleAsync(locals.pool.connect(getManyOrganizationalUnitContainers({})))
-	]);
-
-	if (url.hostname === new URL(env.PUBLIC_BASE_URL ?? '').hostname) {
-		currentOrganization = organizations.find(({ payload }) => payload.default);
-		if (!currentOrganization) {
-			currentOrganization = (await locals.pool.connect(
-				setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
-			)) as OrganizationContainer;
+		async function filterVisibleAsync<T extends AnyContainer>(promise: Promise<Array<T>>) {
+			const containers = await promise;
+			return filterVisible(containers, locals.user);
 		}
-	} else {
-		currentOrganization = organizations.find(({ guid }) => url.hostname.startsWith(`${guid}.`));
-	}
 
-	if (!currentOrganization) {
-		currentOrganizationalUnit = organizationalUnits.find(({ guid }) =>
-			url.hostname.startsWith(`${guid}.`)
-		);
+		const [organizations, organizationalUnits] = await Promise.all([
+			filterVisibleAsync(locals.pool.connect(getManyOrganizationContainers({}, 'alpha'))),
+			filterVisibleAsync(locals.pool.connect(getManyOrganizationalUnitContainers({})))
+		]);
 
-		currentOrganization = organizations.find(
-			({ guid }) => guid === currentOrganizationalUnit?.organization
-		);
-	}
-
-	if (!currentOrganization) {
-		error(404, { message: unwrapFunctionStore(_)('error.not_found') });
-	}
-
-	if (url.searchParams.has('signup')) {
-		try {
-			const foundUser = await findUserById(url.searchParams.get('signup') as string);
-			if (!foundUser.emailVerified) {
-				user = foundUser;
+		if (url.hostname === new URL(env.PUBLIC_BASE_URL ?? '').hostname) {
+			currentOrganization = organizations.find(({ payload }) => payload.default);
+			if (!currentOrganization) {
+				currentOrganization = (await locals.pool.connect(
+					setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
+				)) as OrganizationContainer;
 			}
-		} catch (error) {
-			log.warn(isErrorLike(error) ? serializeError(error) : {}, String(error));
+		} else {
+			currentOrganization = organizations.find(({ guid }) => url.hostname.startsWith(`${guid}.`));
 		}
-	}
 
-	const random = await fetch('/random');
+		if (!currentOrganization) {
+			currentOrganizationalUnit = organizationalUnits.find(({ guid }) =>
+				url.hostname.startsWith(`${guid}.`)
+			);
 
-	return {
-		currentOrganization,
-		currentOrganizationalUnit,
-		features: locals.features,
-		organizations,
-		organizationalUnits,
-		random: await random.json(),
-		session: await locals.auth(),
-		user
-	};
-};
+			currentOrganization = organizations.find(
+				({ guid }) => guid === currentOrganizationalUnit?.organization
+			);
+		}
+
+		if (!currentOrganization) {
+			error(404, { message: unwrapFunctionStore(_)('error.not_found') });
+		}
+
+		if (url.searchParams.has('signup')) {
+			try {
+				const foundUser = await findUserById(url.searchParams.get('signup') as string);
+				if (!foundUser.emailVerified) {
+					user = foundUser;
+				}
+			} catch (error) {
+				log.warn(isErrorLike(error) ? serializeError(error) : {}, String(error));
+			}
+		}
+
+		const random = await fetch('/random');
+
+		span.end();
+
+		return {
+			currentOrganization,
+			currentOrganizationalUnit,
+			features: locals.features,
+			organizations,
+			organizationalUnits,
+			random: await random.json(),
+			session: await locals.auth(),
+			user
+		};
+	});
--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,3 +1,4 @@
+import { trace, type Span } from '@opentelemetry/api';
 import { filterVisible } from '$lib/authorization';
 import { audience, filterOrganizationalUnits, payloadTypes, predicates } from '$lib/models';
 import {
@@ -9,139 +10,144 @@ import {
 } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ locals, url, parent }) => {
-	let containers;
-	let containersWithIndicatorContributions;
-	let subordinateOrganizationalUnits: string[] = [];
-	const { currentOrganization, currentOrganizationalUnit } = await parent();
+const tracer = trace.getTracer('app');
 
-	if (currentOrganizationalUnit) {
-		const relatedOrganizationalUnits = await locals.pool.connect(
-			getAllRelatedOrganizationalUnitContainers(currentOrganizationalUnit.guid)
-		);
-		subordinateOrganizationalUnits = relatedOrganizationalUnits
-			.filter(({ payload }) => payload.level > currentOrganizationalUnit.payload.level)
-			.map(({ guid }) => guid);
-	}
+export const load = (async ({ locals, url, parent }) =>
+	tracer.startActiveSpan('page.load', async (span: Span) => {
+		let containers;
+		let containersWithIndicatorContributions;
+		let subordinateOrganizationalUnits: string[] = [];
+		const { currentOrganization, currentOrganizationalUnit } = await parent();
 
-	if (url.searchParams.has('related-to')) {
-		[containers, containersWithIndicatorContributions] = await Promise.all([
-			locals.pool.connect(
-				getAllRelatedContainers(
-					currentOrganization.payload.default ? [] : [currentOrganization.guid],
-					url.searchParams.get('related-to') as string,
-					url.searchParams.getAll('relationType').length == 0
-						? [
-								predicates.enum['contributes-to'],
-								predicates.enum['is-consistent-with'],
-								predicates.enum['is-equivalent-to'],
-								predicates.enum['is-inconsistent-with'],
-								predicates.enum['is-part-of']
+		if (currentOrganizationalUnit) {
+			const relatedOrganizationalUnits = await locals.pool.connect(
+				getAllRelatedOrganizationalUnitContainers(currentOrganizationalUnit.guid)
+			);
+			subordinateOrganizationalUnits = relatedOrganizationalUnits
+				.filter(({ payload }) => payload.level > currentOrganizationalUnit.payload.level)
+				.map(({ guid }) => guid);
+		}
+
+		if (url.searchParams.has('related-to')) {
+			[containers, containersWithIndicatorContributions] = await Promise.all([
+				locals.pool.connect(
+					getAllRelatedContainers(
+						currentOrganization.payload.default ? [] : [currentOrganization.guid],
+						url.searchParams.get('related-to') as string,
+						url.searchParams.getAll('relationType').length == 0
+							? [
+									predicates.enum['contributes-to'],
+									predicates.enum['is-consistent-with'],
+									predicates.enum['is-equivalent-to'],
+									predicates.enum['is-inconsistent-with'],
+									predicates.enum['is-part-of']
+								]
+							: url.searchParams.getAll('relationType'),
+						{
+							type: [
+								payloadTypes.enum.effect,
+								payloadTypes.enum.indicator,
+								payloadTypes.enum.measure,
+								payloadTypes.enum.model,
+								payloadTypes.enum.operational_goal,
+								payloadTypes.enum.simple_measure,
+								payloadTypes.enum.strategic_goal,
+								payloadTypes.enum.strategy,
+								payloadTypes.enum.vision
 							]
-						: url.searchParams.getAll('relationType'),
-					{
-						type: [
-							payloadTypes.enum.effect,
-							payloadTypes.enum.indicator,
-							payloadTypes.enum.measure,
-							payloadTypes.enum.model,
-							payloadTypes.enum.operational_goal,
-							payloadTypes.enum.simple_measure,
-							payloadTypes.enum.strategic_goal,
-							payloadTypes.enum.strategy,
-							payloadTypes.enum.vision
-						]
-					},
-					url.searchParams.get('sort') ?? ''
+						},
+						url.searchParams.get('sort') ?? ''
+					)
+				),
+				locals.pool.connect(
+					getAllContainersWithIndicatorContributions(
+						currentOrganization.payload.default ? [] : [currentOrganization.guid]
+					)
 				)
-			),
-			locals.pool.connect(
-				getAllContainersWithIndicatorContributions(
-					currentOrganization.payload.default ? [] : [currentOrganization.guid]
+			]);
+		} else if (url.searchParams.has('strategyType')) {
+			[containers, containersWithIndicatorContributions] = await Promise.all([
+				locals.pool.connect(
+					getAllRelatedContainersByStrategyType(
+						currentOrganization.payload.default ? [] : [currentOrganization.guid],
+						url.searchParams.getAll('strategyType'),
+						{
+							audience: url.searchParams.has('audienceChanged')
+								? url.searchParams.getAll('audience')
+								: [audience.enum['audience.public'], audience.enum['audience.organization']],
+							categories: url.searchParams.getAll('category'),
+							topics: url.searchParams.getAll('topic'),
+							terms: url.searchParams.get('terms') ?? '',
+							type: [
+								payloadTypes.enum.effect,
+								payloadTypes.enum.indicator,
+								payloadTypes.enum.measure,
+								payloadTypes.enum.model,
+								payloadTypes.enum.operational_goal,
+								payloadTypes.enum.simple_measure,
+								payloadTypes.enum.strategic_goal,
+								payloadTypes.enum.strategy,
+								payloadTypes.enum.vision
+							]
+						},
+						url.searchParams.get('sort') ?? ''
+					)
+				),
+				locals.pool.connect(
+					getAllContainersWithIndicatorContributions(
+						currentOrganization.payload.default ? [] : [currentOrganization.guid]
+					)
 				)
-			)
-		]);
-	} else if (url.searchParams.has('strategyType')) {
-		[containers, containersWithIndicatorContributions] = await Promise.all([
-			locals.pool.connect(
-				getAllRelatedContainersByStrategyType(
-					currentOrganization.payload.default ? [] : [currentOrganization.guid],
-					url.searchParams.getAll('strategyType'),
-					{
-						audience: url.searchParams.has('audienceChanged')
-							? url.searchParams.getAll('audience')
-							: [audience.enum['audience.public'], audience.enum['audience.organization']],
-						categories: url.searchParams.getAll('category'),
-						topics: url.searchParams.getAll('topic'),
-						terms: url.searchParams.get('terms') ?? '',
-						type: [
-							payloadTypes.enum.effect,
-							payloadTypes.enum.indicator,
-							payloadTypes.enum.measure,
-							payloadTypes.enum.model,
-							payloadTypes.enum.operational_goal,
-							payloadTypes.enum.simple_measure,
-							payloadTypes.enum.strategic_goal,
-							payloadTypes.enum.strategy,
-							payloadTypes.enum.vision
-						]
-					},
-					url.searchParams.get('sort') ?? ''
+			]);
+		} else {
+			[containers, containersWithIndicatorContributions] = await Promise.all([
+				locals.pool.connect(
+					getManyContainers(
+						currentOrganization.payload.default ? [] : [currentOrganization.guid],
+						{
+							audience: url.searchParams.has('audienceChanged')
+								? url.searchParams.getAll('audience')
+								: [audience.enum['audience.public'], audience.enum['audience.organization']],
+							categories: url.searchParams.getAll('category'),
+							topics: url.searchParams.getAll('topic'),
+							strategyTypes: url.searchParams.getAll('strategyType'),
+							terms: url.searchParams.get('terms') ?? '',
+							type: [
+								payloadTypes.enum.effect,
+								payloadTypes.enum.indicator,
+								payloadTypes.enum.measure,
+								payloadTypes.enum.model,
+								payloadTypes.enum.operational_goal,
+								payloadTypes.enum.resolution,
+								payloadTypes.enum.simple_measure,
+								payloadTypes.enum.strategic_goal,
+								payloadTypes.enum.strategy,
+								payloadTypes.enum.vision
+							]
+						},
+						url.searchParams.get('sort') ?? ''
+					)
+				),
+				locals.pool.connect(
+					getAllContainersWithIndicatorContributions(
+						currentOrganization.payload.default ? [] : [currentOrganization.guid]
+					)
 				)
-			),
-			locals.pool.connect(
-				getAllContainersWithIndicatorContributions(
-					currentOrganization.payload.default ? [] : [currentOrganization.guid]
-				)
-			)
-		]);
-	} else {
-		[containers, containersWithIndicatorContributions] = await Promise.all([
-			locals.pool.connect(
-				getManyContainers(
-					currentOrganization.payload.default ? [] : [currentOrganization.guid],
-					{
-						audience: url.searchParams.has('audienceChanged')
-							? url.searchParams.getAll('audience')
-							: [audience.enum['audience.public'], audience.enum['audience.organization']],
-						categories: url.searchParams.getAll('category'),
-						topics: url.searchParams.getAll('topic'),
-						strategyTypes: url.searchParams.getAll('strategyType'),
-						terms: url.searchParams.get('terms') ?? '',
-						type: [
-							payloadTypes.enum.effect,
-							payloadTypes.enum.indicator,
-							payloadTypes.enum.measure,
-							payloadTypes.enum.model,
-							payloadTypes.enum.operational_goal,
-							payloadTypes.enum.resolution,
-							payloadTypes.enum.simple_measure,
-							payloadTypes.enum.strategic_goal,
-							payloadTypes.enum.strategy,
-							payloadTypes.enum.vision
-						]
-					},
-					url.searchParams.get('sort') ?? ''
-				)
-			),
-			locals.pool.connect(
-				getAllContainersWithIndicatorContributions(
-					currentOrganization.payload.default ? [] : [currentOrganization.guid]
-				)
-			)
-		]);
-	}
+			]);
+		}
 
-	return {
-		containers: filterOrganizationalUnits(
-			filterVisible(containers, locals.user),
-			url,
-			subordinateOrganizationalUnits,
-			currentOrganizationalUnit
-		),
-		containersWithIndicatorContributions: filterVisible(
-			containersWithIndicatorContributions,
-			locals.user
-		)
-	};
-}) satisfies PageServerLoad;
+		span.end();
+
+		return {
+			containers: filterOrganizationalUnits(
+				filterVisible(containers, locals.user),
+				url,
+				subordinateOrganizationalUnits,
+				currentOrganizationalUnit
+			),
+			containersWithIndicatorContributions: filterVisible(
+				containersWithIndicatorContributions,
+				locals.user
+			)
+		};
+	})) satisfies PageServerLoad;
