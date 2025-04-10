5b45266870e9ff352ea87acb7085c72986b33e2e
Stefan Freudenberg <stefan@agaric.com>
2025-04-10 09:40:21 +0200

    Introduce compact prop to category and topic drop-down components

--- app/src/lib/components/CategoryDropdown.svelte
+++ app/src/lib/components/CategoryDropdown.svelte
@@ -7,12 +7,13 @@
 	import { sdgIcons } from '$lib/theme/models';
 
 	interface Props {
+		compact?: boolean;
 		editable?: boolean;
 		handleChange: (event: Event) => void;
 		value: SustainableDevelopmentGoal[];
 	}
 
-	let { editable = false, handleChange, value = $bindable() }: Props = $props();
+	let { compact = false, editable = false, handleChange, value = $bindable() }: Props = $props();
 
 	const popover = createPopover({ label: $_('category') });
 
@@ -22,24 +23,29 @@
 	});
 
 	const extraOpts = $derived({
-		modifiers: [{ name: 'offset', options: { offset: [editable ? -45 : -13, -43] } }]
+		modifiers: [
+			{
+				name: 'offset',
+				options: { offset: [compact ? (editable ? -45 : -13) : 0, compact ? -43 : 4] }
+			}
+		]
 	});
 </script>
 
-{#if editable || value.length > 6}
+{#if editable || (value.length > 6 && compact)}
 	<div class="dropdown" use:popperRef>
 		<button class="dropdown-button" type="button" use:popover.button>
-			<span class="selected">
+			<span class="value">
 				{#each sustainableDevelopmentGoals.options
 					.filter((o) => value.includes(o))
-					.slice(0, value.length > 6 ? 5 : value.length)
+					.slice(0, value.length > 6 && compact ? 5 : value.length)
 					.map((o) => ({ label: $_(o), value: o })) as selectedOption}
 					{@const sdgIcon = sdgIcons.get(selectedOption.value)}
 					<img src={sdgIcon} width="30" height="30" alt={selectedOption.label} />
 				{:else}
 					{$_('empty')}
 				{/each}
-				{#if value.length > 6}
+				{#if value.length > 6 && compact}
 					<span class="more-indicator">
 						{$_('n_more', { values: { count: value.length - 5 } })}
 					</span>
@@ -69,7 +75,7 @@
 				</fieldset>
 			{:else if value.length > 0}
 				<div class="dropdown-panel" use:popperContent={extraOpts} use:popover.panel>
-					<ul>
+					<ul class="value">
 						{#each value as category}
 							<li>
 								<img
@@ -105,12 +111,6 @@
 {/if}
 
 <style>
-	ul {
-		display: flex;
-		flex-wrap: wrap;
-		gap: 0.5rem;
-	}
-
 	.dropdown-panel {
 		padding: 0.75rem;
 	}
@@ -128,14 +128,15 @@
 		width: 30px;
 	}
 
-	.selected {
+	.value {
 		display: flex;
+		flex-wrap: wrap;
 		gap: 0.5rem;
 	}
 
 	@container style(--drop-down-style: table) {
-		ul,
-		.dropdown-button {
+		.dropdown-button,
+		.value {
 			width: 13.75rem;
 		}
 	}
