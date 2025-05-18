34fab3778d23e059ffe3d13754c4794af15c170f
Stefan Freudenberg <stefan@agaric.com>
2025-05-18 10:01:02 +0200

    Migrate Navigation component to Svelte 5

--- app/src/lib/components/Navigation.svelte
+++ app/src/lib/components/Navigation.svelte
@@ -3,14 +3,16 @@
 	import { _ } from 'svelte-i18n';
 	import Share from '~icons/heroicons/share-20-solid';
 	import Members from '~icons/knotdots/members';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import OrganizationMenu from '$lib/components/OrganizationMenu.svelte';
 	import { popover } from '$lib/components/OrganizationMenu.svelte';
 	import Workspaces from '$lib/components/Workspaces.svelte';
 	import { boards } from '$lib/models';
 	import { ability, user } from '$lib/stores';
 
-	$: selectedContext = $page.data.currentOrganizationalUnit ?? $page.data.currentOrganization;
+	let selectedContext = $derived(
+		page.data.currentOrganizationalUnit ?? page.data.currentOrganization
+	);
 </script>
 
 <nav class:is-elevated={$popover.expanded} data-sveltekit-preload-data="hover">
@@ -20,7 +22,7 @@
 		<a
 			href="/"
 			class="button button-nav"
-			class:is-active={$page.url.pathname === '/'}
+			class:is-active={page.url.pathname === '/'}
 			title={$_('board.elements')}
 		>
 			<Share />
@@ -33,22 +35,22 @@
 		/>
 	</div>
 
-	{#if $page.data.currentOrganizationalUnit && $ability.can('invite-members', $page.data.currentOrganizationalUnit)}
+	{#if page.data.currentOrganizationalUnit && $ability.can('invite-members', page.data.currentOrganizationalUnit)}
 		<a
-			href="/organization/{$page.data.currentOrganizationalUnit.guid}/members"
+			href="/organization/{page.data.currentOrganizationalUnit.guid}/members"
 			class="button button-nav"
-			class:is-active={$page.url.pathname ===
-				`/organization/${$page.data.currentOrganizationalUnit.guid}/members`}
+			class:is-active={page.url.pathname ===
+				`/organization/${page.data.currentOrganizationalUnit.guid}/members`}
 			title={$_('members')}
 		>
 			<Members />
 		</a>
-	{:else if !$page.data.currentOrganization.payload.default && $ability.can('invite-members', $page.data.currentOrganization)}
+	{:else if !page.data.currentOrganization.payload.default && $ability.can('invite-members', page.data.currentOrganization)}
 		<a
-			href="/organizational_unit/{$page.data.currentOrganization.guid}/members"
+			href="/organizational_unit/{page.data.currentOrganization.guid}/members"
 			class="button button-nav"
-			class:is-active={$page.url.pathname ===
-				`/organizational_unit/${$page.data.currentOrganization.guid}/members`}
+			class:is-active={page.url.pathname ===
+				`/organizational_unit/${page.data.currentOrganization.guid}/members`}
 			title={$_('members')}
 		>
 			<Members />
