9c4139d1ca61514bb148ef20f6f2eca59782e1c4
Stefan Freudenberg <stefan@agaric.com>
2025-05-26 20:40:07 +0200

    Add catalog and table view to all workspaces

---
+++ app/src/routes/all/catalog/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import AllPage from '$lib/components/AllPage.svelte';
+	import Catalog from '$lib/components/Catalog.svelte';
+	import Help from '$lib/components/Help.svelte';
+	import {
+		isGoalContainer,
+		isMeasureContainer,
+		isResolutionContainer,
+		isSimpleMeasureContainer,
+		isStrategyContainer
+	} from '$lib/models';
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
+<AllPage {data}>
+	<Catalog
+		containers={data.containers
+			.filter(
+				(c) =>
+					isGoalContainer(c) ||
+					isMeasureContainer(c) ||
+					isResolutionContainer(c) ||
+					isSimpleMeasureContainer(c) ||
+					isStrategyContainer(c)
+			)
+			.slice(0, browser ? undefined : 20)}
+	/>
+	<Help slug="all" />
+</AllPage>
---
+++ app/src/routes/all/table/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import { browser } from '$app/environment';
+	import AllPage from '$lib/components/AllPage.svelte';
+	import Help from '$lib/components/Help.svelte';
+	import type { PageProps } from './$types';
+	import Table from '$lib/components/Table.svelte';
+	import {
+		isGoalContainer,
+		isMeasureContainer,
+		isResolutionContainer,
+		isSimpleMeasureContainer,
+		isStrategyContainer
+	} from '$lib/models';
+
+	let { data }: PageProps = $props();
+</script>
+
+<AllPage {data}>
+	<Table
+		columns={[
+			{ heading: $_('title'), key: 'title' },
+			{ heading: $_('object'), key: 'type' },
+			{ heading: $_('description'), key: 'description' },
+			{ heading: $_('visibility.label'), key: 'visibility' },
+			{ heading: $_('status'), key: 'status' },
+			{ heading: $_('category'), key: 'category' },
+			{ heading: $_('topic'), key: 'topic' },
+			{ heading: $_('policy_field_bnk'), key: 'policyFieldBNK' },
+			{ heading: $_('audience'), key: 'audience' },
+			{ heading: $_('fulfillment_date'), key: 'fulfillmentDate' },
+			{ heading: $_('planned_duration'), key: 'duration' },
+			{ heading: $_('organizational_unit'), key: 'organizationalUnit' }
+		]}
+		rows={data.containers
+			.filter(
+				(c) =>
+					isGoalContainer(c) ||
+					isMeasureContainer(c) ||
+					isResolutionContainer(c) ||
+					isSimpleMeasureContainer(c) ||
+					isStrategyContainer(c)
+			)
+			.slice(0, browser ? undefined : 20)}
+	/>
+	<Help slug="all-table" />
+</AllPage>
