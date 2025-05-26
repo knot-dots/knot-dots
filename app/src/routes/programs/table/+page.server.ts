417c6075e2687fb7b7bac6d418d91c5854be2308
Stefan Freudenberg <stefan@agaric.com>
2025-05-26 11:59:10 +0200

    Extract component and load function

---
+++ app/src/routes/programs/table/+page.server.ts
@@ -0,0 +1,3 @@
+import programs from '$lib/load/programs';
+
+export const load = async (event) => programs(event);
--- app/src/routes/programs/table/+page.svelte
+++ app/src/routes/programs/table/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import Catalog from '$lib/components/Catalog.svelte';
 	import Help from '$lib/components/Help.svelte';
-	import ProgramsPage from '$lib/components/ProgramsPage.svelte';
+
 	import type { PageProps } from './$types';
+	import ProgramsPage from '$lib/components/ProgramsPage.svelte';
 
 	let { data }: PageProps = $props();
 </script>
