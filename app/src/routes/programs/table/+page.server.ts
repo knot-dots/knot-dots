195b9e363329602188796e7a25930f72c0d79469
Stefan Freudenberg <stefan@agaric.com>
2025-05-26 12:17:11 +0200

    Add table view to programs workspaces

--- app/src/routes/programs/table/+page.server.ts
+++ app/src/routes/programs/table/+page.server.ts
@@ -1,3 +1,4 @@
 import programs from '$lib/load/programs';
+import type { PageServerLoad } from '../$types';
 
-export const load = async (event) => programs(event);
+export const load = programs satisfies PageServerLoad;
--- app/src/routes/programs/table/+page.svelte
+++ app/src/routes/programs/table/+page.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-	import Catalog from '$lib/components/Catalog.svelte';
+	import { _ } from 'svelte-i18n';
 	import Help from '$lib/components/Help.svelte';
-
-	import type { PageProps } from './$types';
 	import ProgramsPage from '$lib/components/ProgramsPage.svelte';
+	import Table from '$lib/components/Table.svelte';
+	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
 </script>
 
 <ProgramsPage {data}>
-	<Catalog containers={data.containers} />
-	<Help slug="strategies" />
+	<Table
+		columns={[
+			{ heading: $_('title'), key: 'title' },
+			{ heading: $_('description'), key: 'description' },
+			{ heading: $_('visibility.label'), key: 'visibility' },
+			{ heading: $_('category'), key: 'category' },
+			{ heading: $_('topic'), key: 'topic' },
+			{ heading: $_('policy_field_bnk'), key: 'policyFieldBNK' },
+			{ heading: $_('audience'), key: 'audience' },
+			{ heading: $_('organizational_unit'), key: 'organizationalUnit' }
+		]}
+		rows={data.containers}
+	/>
+	<Help slug="programs-table" />
 </ProgramsPage>
