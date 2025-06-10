11e9857624df9ab0848fc7d028946cdbdab6d149
Stefan Freudenberg <stefan@agaric.com>
2025-06-10 15:15:48 +0200

    Fix empty carousels on landing pages of organizational units

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,30 +1,56 @@
 import { filterVisible } from '$lib/authorization';
-import { type IndicatorContainer, payloadTypes } from '$lib/models';
-import { getAllContainersRelatedToIndicators, getManyContainers } from '$lib/server/db';
+import { type IndicatorContainer, isOrganizationalUnitContainer, payloadTypes } from '$lib/models';
+import {
+	getAllContainersRelatedToIndicators,
+	getAllRelatedOrganizationalUnitContainers,
+	getManyContainers
+} from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ url, locals, parent }) => {
+export const load = (async ({ locals, parent }) => {
 	const { currentOrganization, currentOrganizationalUnit } = await parent();
 	const container = currentOrganizationalUnit ?? currentOrganization;
+	let organizationalUnits: string[] = [];
+
+	if (isOrganizationalUnitContainer(container)) {
+		const relatedOrganizationalUnits = await locals.pool.connect(
+			getAllRelatedOrganizationalUnitContainers(container.guid)
+		);
+		organizationalUnits = relatedOrganizationalUnits
+			.filter(({ payload }) => payload.level > container.payload.level)
+			.map(({ guid }) => guid)
+			.concat(container.guid);
+	}
 
 	const [strategies, measures, indicators] = await Promise.all([
 		locals.pool.connect(
-			getManyContainers([container.guid], { type: [payloadTypes.enum.strategy] }, '')
-		),
-		locals.pool.connect(
 			getManyContainers(
-				[container.guid],
-				{ type: [payloadTypes.enum.measure, payloadTypes.enum.simple_measure] },
+				[container.organization],
+				{ organizationalUnits, type: [payloadTypes.enum.strategy] },
 				''
 			)
 		),
 		locals.pool.connect(
-			getManyContainers([container.guid], { type: [payloadTypes.enum.indicator] }, '')
+			getManyContainers(
+				[container.organization],
+				{
+					organizationalUnits,
+					type: [payloadTypes.enum.measure, payloadTypes.enum.simple_measure]
+				},
+				''
+			)
+		),
+		locals.pool.connect(
+			getManyContainers(
+				[container.organization],
+				{ organizationalUnits, type: [payloadTypes.enum.indicator] },
+				''
+			)
 		)
 	]);
 
 	const relatedContainers = await locals.pool.connect(
-		getAllContainersRelatedToIndicators(indicators as IndicatorContainer[], {})
+		getAllContainersRelatedToIndicators(indicators as IndicatorContainer[], { organizationalUnits })
 	);
 
 	return {
