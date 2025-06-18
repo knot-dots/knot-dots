25807200340756ee2787299fa9fe3f5ffd3129f9
Stefan Freudenberg <stefan@agaric.com>
2025-06-18 10:56:12 +0200

    Enable new editing experience for help pages

--- app/src/lib/components/ViewHelpOverlay.svelte
+++ app/src/lib/components/ViewHelpOverlay.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import Header from '$lib/components/Header.svelte';
 	import PageDetailView from '$lib/components/PageDetailView.svelte';
+	import { createFeatureDecisions } from '$lib/features';
 	import { type PageContainer } from '$lib/models';
 
 	interface Props {
@@ -12,7 +14,13 @@
 
 <Header sortOptions={[]} workspaceOptions={[]} />
 <div class="content-details masked-overflow">
-	<PageDetailView {container} />
+	{#if createFeatureDecisions(page.data.features).useEditableDetailView()}
+		{#await import('./EditablePageDetailView.svelte') then { default: EditablePageDetailView }}
+			<EditablePageDetailView {container} revisions={[container]} />
+		{/await}
+	{:else}
+		<PageDetailView {container} />
+	{/if}
 </div>
 <footer class="content-footer">
 	<div class="content-actions"></div>
