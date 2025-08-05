7b0e6d598b1f37ffd813e91636ab218c04376482
Stefan Freudenberg <stefan@agaric.com>
2025-08-05 09:52:32 +0200

    Move declarations to global CSS

--- app/src/lib/components/Progress.svelte
+++ app/src/lib/components/Progress.svelte
@@ -18,31 +18,6 @@
 {/if}
 
 <style>
-	progress {
-		--height: 6px;
-
-		appearance: none;
-		background-color: var(--color-gray-200);
-		border: none;
-		border-radius: calc(var(--height) * 0.5);
-		height: var(--height);
-		margin-right: 1rem;
-		overflow: hidden;
-		vertical-align: middle;
-	}
-
-	progress::-webkit-progress-bar {
-		background-color: inherit;
-	}
-
-	progress::-webkit-progress-value {
-		background-color: var(--color, var(--color-gray-200));
-	}
-
-	progress::-moz-progress-bar {
-		background-color: var(--color, var(--color-gray-200));
-	}
-
 	.compact {
 		width: 100%;
 	}
