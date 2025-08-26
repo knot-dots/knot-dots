64df61095575c12a1dc4357bce266e3cc55017e2
Stefan Freudenberg <stefan@agaric.com>
2025-08-26 08:02:10 +0200

    Add Matomo Tag Manager script

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import { overrideItemIdKeyNameBeforeInitialisingDndZones } from 'svelte-dnd-action';
 	import { _ } from 'svelte-i18n';
+	import { env } from '$env/dynamic/public';
 	import SignupDialog from '$lib/components/SignupDialog.svelte';
 	import '../app.css';
 	import type { LayoutData } from './$types';
@@ -22,6 +23,17 @@
 
 <svelte:head>
 	<title>{$_('page_title')}</title>
+
+	{#if env.PUBLIC_MATOMO_CONTAINER_ID}
+		{@html `<script>
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  (function() {
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/knotdots.matomo.cloud/container_${env.PUBLIC_MATOMO_CONTAINER_ID}.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>`}
+	{/if}
 </svelte:head>
 
 <slot />
