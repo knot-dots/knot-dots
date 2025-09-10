c6c813ad70ac9444eb57a8f73b69d0450fb5d419
Stefan Freudenberg <stefan@agaric.com>
2025-09-10 18:37:06 +0200

    Format source code

--- migrate/sql/20250903200852_add_import_tables.up.sql
+++ migrate/sql/20250903200852_add_import_tables.up.sql
@@ -1,26 +1,26 @@
 CREATE EXTENSION postgis;
 
 CREATE TABLE administrative_area_bbsr (
-                                          area float NOT NULL,
-                                          name varchar(1000) NOT NULL,
-                                          official_municipality_key varchar(8) NOT NULL,
-                                          official_regional_code varchar(12) NOT NULL,
-                                          population int NOT NULL,
-                                          city_and_municipality_type varchar(20) NOT NULL,
-                                          valid_from timestamp with time zone default now()
+      area float NOT NULL,
+      name varchar(1000) NOT NULL,
+      official_municipality_key varchar(8) NOT NULL,
+      official_regional_code varchar(12) NOT NULL,
+      population int NOT NULL,
+      city_and_municipality_type varchar(20) NOT NULL,
+      valid_from timestamp with time zone default now()
 );
 
 CREATE UNIQUE INDEX administrative_area_bbsr_omk_key ON administrative_area_bbsr (official_municipality_key);
 CREATE UNIQUE INDEX administrative_area_bbsr_orc_key ON administrative_area_bbsr (official_regional_code);
 
 CREATE TABLE administrative_area_open_street_map (
-                                                     boundary geometry,
-                                                     name varchar(1000) NOT NULL,
-                                                     official_municipality_key varchar(8),
-                                                     official_regional_code varchar(12),
-                                                     relation_id int NOT NULL,
-                                                     wikidata_id varchar(100),
-                                                     valid_from timestamp with time zone default now()
+     boundary geometry,
+     name varchar(1000) NOT NULL,
+     official_municipality_key varchar(8),
+     official_regional_code varchar(12),
+     relation_id int NOT NULL,
+     wikidata_id varchar(100),
+     valid_from timestamp with time zone default now()
 );
 
 CREATE UNIQUE INDEX administrative_area_osm_omk_key ON administrative_area_open_street_map (official_municipality_key, valid_from);
@@ -29,16 +29,16 @@ CREATE UNIQUE INDEX administrative_area_osm_relation_id_key ON administrative_ar
 CREATE UNIQUE INDEX administrative_area_osm_wikidata_id_key ON administrative_area_open_street_map (wikidata_id, valid_from);
 
 CREATE TABLE administrative_area_wikidata (
-                                              area float,
-                                              coat_of_arms varchar(1000),
-                                              country char(2) NOT NULL,
-                                              id varchar(100) NOT NULL,
-                                              name varchar(1000) NOT NULL,
-                                              official_municipality_key varchar(8),
-                                              official_regional_code varchar(12),
-                                              open_street_map_relation_id int NOT NULL,
-                                              population int,
-                                              valid_from timestamp with time zone default now()
+      area float,
+      coat_of_arms varchar(1000),
+      country char(2) NOT NULL,
+      id varchar(100) NOT NULL,
+      name varchar(1000) NOT NULL,
+      official_municipality_key varchar(8),
+      official_regional_code varchar(12),
+      open_street_map_relation_id int NOT NULL,
+      population int,
+      valid_from timestamp with time zone default now()
 );
 
 CREATE UNIQUE INDEX administrative_area_wd_id_key ON administrative_area_wikidata (id, valid_from);
