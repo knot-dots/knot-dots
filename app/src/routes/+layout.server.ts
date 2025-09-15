b995f542e9c8336c9bb8a08226654002b0cfedc3
Stefan Freudenberg <stefan@agaric.com>
2025-09-15 19:12:26 +0200

    Remove custom tracing

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -1,4 +1,3 @@
-import { trace, type Span } from '@opentelemetry/api';
 import { error } from '@sveltejs/kit';
 import { Roarr as log } from 'roarr';
 import { isErrorLike, serializeError } from 'serialize-error';
@@ -15,57 +14,52 @@ import {
 import { findUserById } from '$lib/server/keycloak';
 import type { LayoutServerLoad } from './$types';
 
-const tracer = trace.getTracer('app');
+export const load: LayoutServerLoad = async ({ locals, url }) => {
+	let currentOrganization;
+	let user: KeycloakUser | undefined = undefined;
 
-export const load: LayoutServerLoad = async ({ locals, url }) =>
-	tracer.startActiveSpan('layout.load', async (span: Span) => {
-		let currentOrganization;
-		let user: KeycloakUser | undefined = undefined;
+	async function filterVisibleAsync<T extends AnyContainer>(promise: Promise<Array<T>>) {
+		const containers = await promise;
+		return filterVisible(containers, locals.user);
+	}
 
-		async function filterVisibleAsync<T extends AnyContainer>(promise: Promise<Array<T>>) {
-			const containers = await promise;
-			return filterVisible(containers, locals.user);
-		}
+	const [organizations, organizationalUnits] = await Promise.all([
+		filterVisibleAsync(locals.pool.connect(getManyOrganizationContainers({}, 'alpha'))),
+		filterVisibleAsync(locals.pool.connect(getManyOrganizationalUnitContainers({})))
+	]);
 
-		const [organizations, organizationalUnits] = await Promise.all([
-			filterVisibleAsync(locals.pool.connect(getManyOrganizationContainers({}, 'alpha'))),
-			filterVisibleAsync(locals.pool.connect(getManyOrganizationalUnitContainers({})))
-		]);
-
-		if (url.hostname === new URL(env.PUBLIC_BASE_URL ?? '').hostname) {
-			currentOrganization = organizations.find(({ payload }) => payload.default);
-			if (!currentOrganization) {
-				currentOrganization = (await locals.pool.connect(
-					setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
-				)) as OrganizationContainer;
-			}
-		} else {
-			currentOrganization = organizations.find(({ guid }) => url.hostname.startsWith(`${guid}.`));
-		}
-
+	if (url.hostname === new URL(env.PUBLIC_BASE_URL ?? '').hostname) {
+		currentOrganization = organizations.find(({ payload }) => payload.default);
 		if (!currentOrganization) {
-			error(404, { message: unwrapFunctionStore(_)('error.not_found') });
+			currentOrganization = (await locals.pool.connect(
+				setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
+			)) as OrganizationContainer;
 		}
+	} else {
+		currentOrganization = organizations.find(({ guid }) => url.hostname.startsWith(`${guid}.`));
+	}
 
-		if (url.searchParams.has('signup')) {
-			try {
-				const foundUser = await findUserById(url.searchParams.get('signup') as string);
-				if (!foundUser.emailVerified) {
-					user = foundUser;
-				}
-			} catch (error) {
-				log.warn(isErrorLike(error) ? serializeError(error) : {}, String(error));
+	if (!currentOrganization) {
+		error(404, { message: unwrapFunctionStore(_)('error.not_found') });
+	}
+
+	if (url.searchParams.has('signup')) {
+		try {
+			const foundUser = await findUserById(url.searchParams.get('signup') as string);
+			if (!foundUser.emailVerified) {
+				user = foundUser;
 			}
+		} catch (error) {
+			log.warn(isErrorLike(error) ? serializeError(error) : {}, String(error));
 		}
+	}
 
-		span.end();
-
-		return {
-			currentOrganization,
-			features: locals.features,
-			organizations,
-			organizationalUnits,
-			session: await locals.auth(),
-			user
-		};
-	});
+	return {
+		currentOrganization,
+		features: locals.features,
+		organizations,
+		organizationalUnits,
+		session: await locals.auth(),
+		user
+	};
+};
