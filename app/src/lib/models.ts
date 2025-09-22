a7d42e8abc974aeb1d790aced337136afa04f6e5
Stefan Freudenberg <stefan@agaric.com>
2025-09-22 16:49:02 +0200

    Add more units

--- app/src/lib/models.ts
+++ app/src/lib/models.ts
@@ -377,25 +377,31 @@ export function isQuantity(value: unknown): value is Quantity {
 }
 
 const unitValues = [
-	'unit.cubic_meter',
 	'unit.euro',
 	'unit.euro_per_capita',
 	'unit.euro_per_square_meter',
+	'unit.kilogram_per_capita',
 	'unit.kilogram_per_hectare',
 	'unit.kilowatt',
+	'unit.watt_per_capita',
 	'unit.kilowatt_hour',
+	'unit.megawatt_hour_per_year',
+	'unit.meter',
 	'unit.kilometer',
-	'unit.liter_per_capita_per_day',
-	'unit.microgram_per_cubic_meter',
 	'unit.n',
 	'unit.percent',
 	'unit.per_1000',
 	'unit.per_100000',
-	'unit.square_kilometer_per_capita',
 	'unit.square_meter',
+	'unit.square_meter_per_capita',
+	'unit.square_kilometer_per_capita',
+	'unit.cubic_meter',
+	'unit.liter_per_capita_per_day',
+	'unit.microgram_per_cubic_meter',
 	'unit.ton',
 	'unit.ton_per_capita',
-	'unit.watt_per_capita'
+	'unit.minute',
+	'unit.year'
 ] as const;
 
 export const units = z.enum(unitValues);
