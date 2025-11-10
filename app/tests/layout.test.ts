9a92730673696aea295b3bba3c2da924d9abd917
Niels Neumann <niels@knotdots.de>
2025-11-10 12:04:49 +0100

    Add editability test for badges and progress bar for unauthorized users

--- app/tests/layout.test.ts
+++ app/tests/layout.test.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '@playwright/test';
+
 test('home screen has expected regions', async ({ page, viewport }) => {
 	await page.goto('/');
 	await expect(page.getByRole('banner')).toBeVisible();
@@ -21,4 +22,45 @@ test('navigation contains expected elements', async ({ page, viewport }) => {
 		).toBeVisible();
 		await expect(page.getByRole('banner').getByRole('link', { name: 'dots' })).toBeVisible();
 	}
+});
+
+test.describe(() => {
+	test.use({ storageState: 'tests/.auth/bob.json' });
+
+	test('badge is not editable by Bob', async ({ page }) => {
+		await page.goto('/');
+		await page.getByRole('button', { name: 'All', exact: true }).click();
+		await page.getByRole('menuitem', { name: 'Goals' }).click();
+		await page.getByRole('article', { name: 'Test goal' }).first().click();
+
+		// Edit switch
+		const overlay = page.locator('.overlay');
+		const banner = overlay.getByRole('banner');
+		await banner.getByRole('checkbox', { name: '', exact: true }).check();
+
+		const badgeList = page.locator('ul.badges');
+		const badgeButtons = badgeList.getByRole('button');
+
+		if ((await badgeButtons.count()) > 0) {
+			await expect(badgeButtons).not.toBeEditable();
+		}
+	});
+
+	test('progress is not editable by Bob', async ({ page }) => {
+		await page.goto('/');
+		await page.getByRole('button', { name: 'All', exact: true }).click();
+		await page.getByRole('menuitem', { name: 'Goals' }).click();
+		await page.getByRole('article', { name: 'Test goal' }).first().click();
+
+		// Edit switch
+		const overlay = page.locator('.overlay');
+		const banner = overlay.getByRole('banner');
+		await banner.getByRole('checkbox', { name: '', exact: true }).check();
+
+		const slider = page.getByRole('slider', { name: 'Progress' });
+
+		if ((await slider.count()) > 0) {
+			await expect(slider).not.toBeEditable();
+		}
+	});
 });
