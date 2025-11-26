16181c1750ec0b63df07b82aa8c377d956724e1f
Niels Neumann <niels@knotdots.de>
2025-11-26 18:07:03 +0100

    Add broken test/setup to instert subordinate goal

--- app/tests/global.setup.ts
+++ app/tests/global.setup.ts
@@ -8,6 +8,14 @@ setup('insert test organization', async ({ page }) => {
 	await page.getByRole('button', { name: 'Save' }).click();
 });
 
+// setup('insert program for test organozation', async ({ page }) => {
+// 	await page.goto('/');
+
+// });
+
+
+
+
 setup('insert test goal', async ({ page }) => {
 	await page.goto('/');
 	await page.getByRole('button', { name: 'All', exact: true }).click();
@@ -25,6 +33,35 @@ setup('insert test program', async ({ page }) => {
 	await page.getByRole('textbox', { name: 'Title' }).fill('Test program');
 	await page.getByRole('button', { name: 'Save' }).click();
 });
+
+setup('insert goal and subordinate goal in program', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'Page' }).click();
+  await page.getByRole('menuitem', { name: 'Level board' }).click();
+  await page.getByRole('article', { name: 'Test program' }).first().click();
+  await page.getByRole('button', { name: 'Chapter' }).click();
+  await page.getByRole('menuitem', { name: 'Goal' }).click();
+  await page.getByRole('textbox', { name: 'Title' }).fill('Test goal 1');
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('button', { name: 'Back' }).click();
+
+  await page.getByRole('button', { name: 'Chapter' }).first().click();
+  await page.getByRole('menuitem', { name: 'Goal' }).click();
+  await page.getByRole('textbox', { name: 'Title' }).fill('Test goal 2');
+
+  await page.getByText("Show all properties").first().click();
+  await page.getByRole('radio', { name: 'Test goal' }).check();
+  await page.locator('label').filter({ hasText: 'Empty' }).click();
+  await page.locator('[id="headlessui-popover:88"]').getByText('Test goal').click();
+  await page.getByText('General Goal type Empty').click();
+  await page.getByText('Categories').click();
+  await page.locator('[id="headlessui-popover:86"]').click();
+  await page.locator('[id="headlessui-popover:89"]').getByText('Test goal').click();
+  await page.getByText('Create new object Save Cancel').click();
+  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('button', { name: 'Show all properties' }).click();
+  await page.getByRole('link', { name: 'Close' }).click();
+})
 
 setup('insert test measure', async ({ page }) => {
 	await page.goto('/');
