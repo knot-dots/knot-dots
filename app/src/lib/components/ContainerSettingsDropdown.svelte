b3358c32f3425cacc75eea15569aba7e264d6c38
Stefan Freudenberg <stefan@knotdots.de>
2025-12-04 19:14:23 +0100

    Remove unused import

--- app/src/lib/components/ContainerSettingsDropdown.svelte
+++ app/src/lib/components/ContainerSettingsDropdown.svelte
@@ -7,7 +7,6 @@
 	import deleteContainer from '$lib/client/deleteContainer';
 	import ConfirmDeleteDialog from '$lib/components/ConfirmDeleteDialog.svelte';
 	import { type AnyContainer, visibility } from '$lib/models';
-	import { sectionOf } from '$lib/relations';
 	import { ability } from '$lib/stores';
 
 	interface Props {
