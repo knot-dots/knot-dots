f1069514d561d4d4470a37cd54b30a1353c4d261
Niels Neumann <niels@knotdots.de>
2025-12-08 16:37:35 +0100

    Add resource catalog page and server load functionality

---
+++ app/src/routes/[[guid=uuid]]/resources/catalog/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Catalog from '$lib/components/Catalog.svelte';
+	import Help from '$lib/components/Help.svelte';
+	import TasksPage from '$lib/components/TasksPage.svelte';
+	import type { PageProps } from './$types';
+	import { payloadTypes } from '$lib/models';
+
+	let { data }: PageProps = $props();
+</script>
+
+<TasksPage {data}>
+	<Catalog containers={data.containers} payloadType={[payloadTypes.enum.resource_v2]} />
+	<Help slug="tasks-catalog" />
+</TasksPage>
