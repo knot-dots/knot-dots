69dc45cd2d180ccbbff7bad6bd4a6cc7212eb204
Stefan Freudenberg <stefan@knotdots.de>
2025-12-30 17:16:11 +0100

    Encapsulate server-side feature handling in module

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -14,7 +14,7 @@ import {
 	getPool,
 	getUser
 } from '$lib/server/db';
-import { storage } from '$lib/server/context';
+import { withFeatures } from '$lib/server/features';
 import { withLogger } from '$lib/server/logger';
 
 const baseURL = new URL(env.PUBLIC_BASE_URL ?? 'http://localhost:5173');
@@ -107,44 +107,47 @@ const { handle: authentication } = SvelteKitAuth({
 	trustHost: true
 });
 
-export const handle = sequence(withLogger, authentication, async ({ event, resolve }) => {
-	const lang = event.request.headers.get('accept-language')?.split(',')[0];
-	locale.set(lang ?? 'de');
+export const handle = sequence(
+	withLogger,
+	authentication,
+	async ({ event, resolve }) => {
+		const lang = event.request.headers.get('accept-language')?.split(',')[0];
+		locale.set(lang ?? 'de');
 
-	event.locals.pool = await getPool();
+		event.locals.pool = await getPool();
 
-	const session = await event.locals.auth();
-	if (session) {
-		event.locals.user = {
-			...session.user,
-			isAuthenticated: true
-		};
-	} else {
-		event.locals.user = {
-			adminOf: [],
-			collaboratorOf: [],
-			familyName: '',
-			givenName: '',
-			guid: '',
-			headOf: [],
-			isAuthenticated: false,
-			memberOf: [],
-			roles: [],
-			settings: {}
-		};
-	}
+		const session = await event.locals.auth();
+		if (session) {
+			event.locals.user = {
+				...session.user,
+				isAuthenticated: true
+			};
+		} else {
+			event.locals.user = {
+				adminOf: [],
+				collaboratorOf: [],
+				familyName: '',
+				givenName: '',
+				guid: '',
+				headOf: [],
+				isAuthenticated: false,
+				memberOf: [],
+				roles: [],
+				settings: {}
+			};
+		}
 
-	if (
-		event.locals.user.isAuthenticated &&
-		event.url.searchParams.has('redirectToProfileIfLoggedIn')
-	) {
-		redirect(302, '/me');
-	}
+		if (
+			event.locals.user.isAuthenticated &&
+			event.url.searchParams.has('redirectToProfileIfLoggedIn')
+		) {
+			redirect(302, '/me');
+		}
 
-	event.locals.features = event.locals.user.settings.features ?? [];
-
-	return storage.run(event.locals, () => resolve(event));
-}) satisfies Handle;
+		return resolve(event);
+	},
+	withFeatures
+) satisfies Handle;
 
 export const handleError = (async ({ error }) => {
 	log.error(isErrorLike(error) ? serializeError(error) : {}, String(error));
