bb2ea0934352873a45c0b03a92f0588a8ac1457b
Stefan Freudenberg <stefan@knotdots.de>
2025-12-31 12:18:07 +0100

    Replace query with param

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -2,11 +2,15 @@ import { error } from '@sveltejs/kit';
 import { Roarr as log } from 'roarr';
 import { isErrorLike, serializeError } from 'serialize-error';
 import { unwrapFunctionStore, _ } from 'svelte-i18n';
-import { z } from 'zod';
 import { env } from '$env/dynamic/public';
 import { filterVisible } from '$lib/authorization';
-import { type AnyContainer, type KeycloakUser, organizationalUnitType } from '$lib/models';
-import type { OrganizationalUnitContainer, OrganizationContainer } from '$lib/models';
+import {
+	type AnyContainer,
+	type KeycloakUser,
+	type OrganizationalUnitContainer,
+	organizationalUnitType,
+	type OrganizationContainer
+} from '$lib/models';
 import {
 	getManyOrganizationalUnitContainers,
 	getManyOrganizationContainers,
@@ -15,7 +19,7 @@ import {
 import { findUserById } from '$lib/server/keycloak';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ locals, url }) => {
+export const load: LayoutServerLoad = async ({ locals, params, url }) => {
 	let currentOrganization;
 	let currentOrganizationalUnit: OrganizationalUnitContainer | undefined;
 	let user: KeycloakUser | undefined = undefined;
@@ -42,15 +46,11 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
 
 	// Don't use subdomains in dev mode if the env var is set
 	if (env.PUBLIC_DONT_USE_SUBDOMAINS) {
-		// Parse GUID from the URL path
-		let guidFromURL = url.pathname.split('/')[1];
-
-		// Check if the parsed part is a valid UUID
-		if (z.uuid().safeParse(guidFromURL).success) {
-			currentOrganization = organizations.find(({ guid }) => guid === guidFromURL);
+		if (params.guid) {
+			currentOrganization = organizations.find(({ guid }) => guid === params.guid);
 
 			if (!currentOrganization) {
-				currentOrganizationalUnit = organizationalUnits.find(({ guid }) => guid === guidFromURL);
+				currentOrganizationalUnit = organizationalUnits.find(({ guid }) => guid === params.guid);
 				currentOrganization = organizations.find(
 					({ guid }) => guid === currentOrganizationalUnit?.organization
 				);
