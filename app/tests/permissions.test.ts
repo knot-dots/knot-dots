17d818eaae57323041c677138ff51d36bc6d9f1c
Stefan Freudenberg <stefan@knotdots.de>
2026-01-11 22:35:11 +0100

    Optimize test

--- app/tests/permissions.test.ts
+++ app/tests/permissions.test.ts
@@ -17,42 +17,44 @@ test.describe('Permissions', () => {
 			suborg2Title = `Suborg 2 ${test.info().project.name} ${Date.now()}`;
 			editableTask = `Editable Task ${test.info().project.name} ${Date.now()}`;
 
+			const orgMenuButton = page.getByRole('button', {
+				name: 'Organizations and organizational units'
+			});
+
 			await page.goto('/');
-			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
+			await orgMenuButton.click();
+			await page.waitForTimeout(150);
 			await page.getByRole('article', { name: testOrganization.payload.name }).click();
-			await page.waitForTimeout(500);
-			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
-			await page.waitForTimeout(500);
+			await expect(
+				page.getByRole('heading', { level: 1, name: testOrganization.payload.name })
+			).toBeVisible();
+			await orgMenuButton.click();
+			await page.waitForTimeout(150);
 			await page
 				.locator('section')
 				.filter({ hasText: 'Level 1' })
 				.getByText('Add item')
 				.first()
 				.click();
-			await page.waitForTimeout(500);
 			await page.getByRole('textbox', { name: 'Title' }).fill(suborgTitle);
 			await page.getByRole('button', { name: 'Save' }).click();
-			await page.waitForTimeout(500);
-			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
-			await page.waitForTimeout(500);
+			await expect(page.getByRole('heading', { level: 1, name: suborgTitle })).toBeVisible();
+			await orgMenuButton.click();
+			await page.waitForTimeout(150);
 			await page
 				.locator('section')
 				.filter({ hasText: 'Level 1' })
 				.getByText('Add item')
 				.first()
 				.click();
-			await page.waitForTimeout(500);
 			await page.getByRole('textbox', { name: 'Title' }).fill(suborg2Title);
 			await page.getByRole('button', { name: 'Save' }).click();
 			await page.goto('/');
-			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
-			await page.waitForTimeout(500);
-			await page.getByRole('link', { name: suborgTitle }).click({ force: true });
-			await page.waitForTimeout(500);
+			await orgMenuButton.click();
+			await page.waitForTimeout(150);
+			await page.getByRole('article', { name: suborgTitle }).click();
 			await page.getByRole('button', { name: 'All', exact: true }).click();
-			await page.waitForTimeout(500);
 			await page.getByRole('menuitem', { name: 'Tasks' }).click();
-			await page.waitForTimeout(500);
 			await page.getByRole('link', { name: 'Add item' }).first().click();
 			await page.getByRole('textbox', { name: 'Title' }).fill(editableTask);
 			await page.getByRole('button', { name: 'Save' }).click();
@@ -63,21 +65,17 @@ test.describe('Permissions', () => {
 			await page.locator('.overlay').getByRole('checkbox', { name: 'Edit mode' }).check();
 			await page.getByRole('button', { name: 'Show all properties' }).click();
 			await page.getByRole('button', { name: 'Organizational unit' }).click();
-			await page.waitForTimeout(500);
 			await page.getByRole('radio', { name: suborg2Title }).check();
+			await page.waitForTimeout(2100);
 			await page.locator('.overlay').getByRole('link', { name: 'Close' }).click();
 			await page.getByRole('button', { name: 'Organizations and' }).click();
 			await page
 				.getByRole('article', { name: testOrganization.payload.name })
 				.getByLabel('show_related_objects')
 				.click();
-			await page.waitForTimeout(500);
 			await page.getByRole('link', { name: suborg2Title }).click();
-			await page.waitForTimeout(500);
 			await page.getByRole('link', { name: 'Members' }).click();
-			await page.waitForTimeout(500);
 			await page.locator('div.details-section .content-actions button.button-primary').click();
-			await page.waitForTimeout(500);
 			await page.getByRole('textbox', { name: 'Email' }).fill('builderbob@bobby.com');
 			await page.getByRole('button', { name: 'Send invitation' }).click();
 			await page.getByRole('combobox').selectOption('role.collaborator');
@@ -91,9 +89,11 @@ test.describe('Permissions', () => {
 			await page.goto('/');
 			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
 			await page.getByRole('link', { name: testOrganization.payload.name }).click();
+			await expect(
+				page.getByRole('heading', { level: 1, name: testOrganization.payload.name })
+			).toBeVisible();
 			await page.getByRole('button', { name: 'All', exact: true }).click();
 			await page.getByRole('menuitem', { name: 'Tasks' }).click();
-			await page.waitForTimeout(500);
 			await page.getByTitle(editableTask).click();
 			await page.locator('.overlay').getByRole('checkbox', { name: 'Edit mode' }).check();
 			const fulfillmentDateInput = page.getByLabel('Fulfillment date');
@@ -108,6 +108,7 @@ test.describe('Permissions', () => {
 			await page.goto('/');
 			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
 			await page.getByTitle(suborgTitle).click();
+			await expect(page.getByRole('heading', { level: 1, name: suborgTitle })).toBeVisible();
 			await page.getByLabel('edit mode').check();
 			await page.getByRole('button', { name: 'Settings' }).click();
 			await page.getByRole('button', { name: `Delete ${suborgTitle}` }).click();
@@ -116,6 +117,7 @@ test.describe('Permissions', () => {
 			await expect(page.getByTitle(suborgTitle)).not.toBeAttached();
 			await page.getByRole('button', { name: 'Organizations and organizational units' }).click();
 			await page.getByTitle(suborg2Title).click();
+			await expect(page.getByRole('heading', { level: 1, name: suborg2Title })).toBeVisible();
 			await page.getByLabel('edit mode').check();
 			await page.getByRole('button', { name: 'Settings' }).click();
 			await page.getByRole('button', { name: `Delete ${suborg2Title}` }).click();
