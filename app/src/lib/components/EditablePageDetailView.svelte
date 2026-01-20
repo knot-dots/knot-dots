7488def0f7d6b008757c0fcde8b318a79c4d4b5c
Stefan Freudenberg <stefan@knotdots.de>
2026-01-20 21:47:03 +0100

    Add EditablePageDetailView component

---
+++ app/src/lib/components/EditablePageDetailView.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import EditableContainerDetailView from '$lib/components/EditableContainerDetailView.svelte';
+	import EditableFormattedText from '$lib/components/EditableFormattedText.svelte';
+	import PageProperties from '$lib/components/PageProperties.svelte';
+	import type { AnyContainer, Container, PageContainer } from '$lib/models';
+	import { ability, applicationState } from '$lib/stores';
+
+	interface Props {
+		container: PageContainer;
+		relatedContainers: Container[];
+		revisions: AnyContainer[];
+	}
+
+	let { container = $bindable(), relatedContainers, revisions }: Props = $props();
+</script>
+
+<EditableContainerDetailView bind:container>
+	{#snippet data()}
+		<PageProperties
+			bind:container
+			editable={$applicationState.containerDetailView.editable && $ability.can('update', container)}
+			{relatedContainers}
+			{revisions}
+		/>
+
+		{#key container.guid}
+			<EditableFormattedText
+				editable={$applicationState.containerDetailView.editable &&
+					$ability.can('update', container)}
+				label={$_('body')}
+				bind:value={container.payload.body}
+			/>
+		{/key}
+	{/snippet}
+</EditableContainerDetailView>
