592b2d9a8b3d68370cb8ee614c926a531a016bd9
Stefan Freudenberg <stefan@knotdots.de>
2026-02-04 21:02:23 +0100

    Remove obsolete markup and CSS

--- app/src/routes/[guid=uuid]/import/+page.svelte
+++ app/src/routes/[guid=uuid]/import/+page.svelte
@@ -11,42 +11,40 @@
 
 <Layout>
 	{#snippet main()}
-		<div class="detail-page-content">
-			<div class="content-details">
-				{#if form?.errors}
-					<ul>
-						{#each form.errors as error (error)}
-							<li>{error}</li>
-						{/each}
-					</ul>
-				{/if}
-				<form class="details" method="POST" enctype="multipart/form-data">
-					<label>
-						{$_('import.csv_file')}
-						<input accept="text/csv" name="csv" type="file" required />
-					</label>
-					{#await data.users then users}
-						{#if users.length > 0}
-							<label>
-								{$_('creator')}
-								<select name="creator" value={users[0].guid}>
-									{#each users as user (user.guid)}
-										<option value={user.guid}>{displayName(user)}</option>
-									{/each}
-									/>
-								</select></label
-							>
-						{/if}
-					{/await}
-					<footer>
-						<button class="button-primary" type="submit" {@attach tooltip($_('import.submit'))}
-							>{$_('import.submit')}</button
+		<div class="content-details">
+			{#if form?.errors}
+				<ul>
+					{#each form.errors as error (error)}
+						<li>{error}</li>
+					{/each}
+				</ul>
+			{/if}
+			<form class="details" method="POST" enctype="multipart/form-data">
+				<label>
+					{$_('import.csv_file')}
+					<input accept="text/csv" name="csv" type="file" required />
+				</label>
+				{#await data.users then users}
+					{#if users.length > 0}
+						<label>
+							{$_('creator')}
+							<select name="creator" value={users[0].guid}>
+								{#each users as user (user.guid)}
+									<option value={user.guid}>{displayName(user)}</option>
+								{/each}
+								/>
+							</select></label
 						>
-					</footer>
-				</form>
-			</div>
-
-			<Help slug="import" />
+					{/if}
+				{/await}
+				<footer>
+					<button class="button-primary" type="submit" {@attach tooltip($_('import.submit'))}
+						>{$_('import.submit')}</button
+					>
+				</footer>
+			</form>
 		</div>
+
+		<Help slug="import" />
 	{/snippet}
 </Layout>
