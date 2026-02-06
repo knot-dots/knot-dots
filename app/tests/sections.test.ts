b9a2e9e24ec5a80029f5e182e474577d84000a34
Stefan Freudenberg <stefan@knotdots.de>
2026-02-06 22:21:29 +0100

    Move test

--- app/tests/sections.test.ts
+++ app/tests/sections.test.ts
@@ -2,7 +2,10 @@ import { expect, test } from './fixtures';
 
 test.use({ storageState: 'tests/.auth/admin.json' });
 
-test('summary can be added and removed', async ({ dotsBoard, testMeasure }) => {
+test('adding and removing a summary sections updates the card', async ({
+	dotsBoard,
+	testMeasure
+}) => {
 	await dotsBoard.goto(`/${testMeasure.organization}`);
 	await dotsBoard.card(testMeasure.payload.title).click();
 	await dotsBoard.overlay.editModeToggle.check();
@@ -31,4 +34,40 @@ test('summary can be added and removed', async ({ dotsBoard, testMeasure }) => {
 	// Verify the card no longer shows the summary
 	await dotsBoard.overlay.closeButton.click();
 	await expect(dotsBoard.card(testMeasure.payload.title)).not.toContainText(summary);
+});
+
+test('adding and removing a progress section updates the card', async ({ dotsBoard, testGoal }) => {
+	await dotsBoard.goto(`/${testGoal.organization}`);
+	await dotsBoard.card(testGoal.payload.title).click();
+	await dotsBoard.overlay.editModeToggle.check();
+
+	// Add a Progress section and set the progress slider to 80%
+	const section = await dotsBoard.overlay.addSection('Progress');
+	const progressSlider = section.getByRole('slider');
+	await expect(progressSlider).toBeVisible();
+	const sliderOffsetWidth = await progressSlider.evaluate((el) => {
+		return el.getBoundingClientRect().width * 0.8;
+	});
+	await progressSlider.hover({ position: { x: sliderOffsetWidth, y: 10 } });
+	await dotsBoard.page.mouse.down();
+	const saveResponse = dotsBoard.page.waitForResponse(/x-sveltekit-invalidated/);
+	await dotsBoard.page.mouse.up();
+	await saveResponse;
+
+	// Verify the card shows a progress bar in the footer
+	await dotsBoard.overlay.closeButton.click();
+	await expect(dotsBoard.card(testGoal.payload.title).getByRole('progressbar')).toBeVisible();
+
+	// Remove the Summary section
+	await dotsBoard.card(testGoal.payload.title).click();
+	await section.hover();
+	await section.getByRole('button', { name: 'Settings' }).click();
+	await section.getByRole('button', { name: 'Delete' }).click();
+	const deleteResponse = dotsBoard.page.waitForResponse(/x-sveltekit-invalidated/);
+	await dotsBoard.page.getByRole('button', { name: /I want to delete/i }).click();
+	await deleteResponse;
+
+	// Verify the goal card no longer shows a progress bar in the footer
+	await dotsBoard.overlay.closeButton.click();
+	await expect(dotsBoard.card(testGoal.payload.title).getByRole('progressbar')).not.toBeVisible();
 });
