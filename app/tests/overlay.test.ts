3d09853b1b225783362b30c45cefb43f95127638
Stefan Freudenberg <stefan@knotdots.de>
2026-02-17 14:17:36 +0100

    Fix edits in overlay not being saved
    When editing objects sharing the same editable detail view component in
    sequence without closing the overlay, only changes made to the first
    were persisted. See also 576bb836c027c109a0e76bc19c02370fd243e2f4.

---
+++ app/tests/overlay.test.ts
@@ -0,0 +1,54 @@
+import { expect, test } from './fixtures';
+
+test.use({ storageState: 'tests/.auth/admin.json' });
+
+test('objects can be edited sequentially', async ({ dotsBoard, testOrganization }) => {
+	await dotsBoard.goto(`/${testOrganization.guid}`);
+
+	// Create two goals
+	const titleOfFirstGoal = 'My first goal';
+	const titleOfSecondGoal = 'My second goal';
+
+	await dotsBoard.column('Goals').addItemButton.click();
+	await dotsBoard.page
+		.getByRole('dialog')
+		.getByRole('textbox', { name: 'Title' })
+		.fill(titleOfFirstGoal);
+	await dotsBoard.page.getByRole('dialog').getByRole('button', { name: 'Save' }).click();
+	await dotsBoard.overlay.closeButton.click();
+
+	await dotsBoard.column('Goals').addItemButton.click();
+	await dotsBoard.page
+		.getByRole('dialog')
+		.getByRole('textbox', { name: 'Title' })
+		.fill(titleOfSecondGoal);
+	await dotsBoard.page.getByRole('dialog').getByRole('button', { name: 'Save' }).click();
+	await dotsBoard.overlay.closeButton.click();
+
+	// Change type of the first goal
+	await dotsBoard.card(titleOfFirstGoal).click();
+	await expect(dotsBoard.overlay.title).toHaveText(titleOfFirstGoal);
+	await dotsBoard.overlay.editModeToggle.check();
+	await expect(dotsBoard.overlay.locator.getByLabel('Goal type')).toHaveText('Empty');
+	const firstInvalidateRequest = dotsBoard.page.waitForRequest(/x-sveltekit-invalidated/);
+	await dotsBoard.overlay.locator.getByLabel('Goal type').click();
+	const typeOfFirstGoal = 'Vision';
+	await dotsBoard.overlay.locator.getByRole('radio', { name: typeOfFirstGoal }).check();
+	await firstInvalidateRequest;
+
+	// Change description of the second goal
+	await dotsBoard.card(titleOfSecondGoal).click();
+	await expect(dotsBoard.overlay.title).toHaveText(titleOfSecondGoal);
+	await expect(dotsBoard.overlay.locator.getByLabel('Goal type')).toHaveText('Empty');
+	const secondInvalidateRequest = dotsBoard.page.waitForRequest(/x-sveltekit-invalidated/);
+	await dotsBoard.overlay.locator.getByLabel('Goal type').click();
+	const typeOfSecondGoal = 'Strategic goal';
+	await dotsBoard.overlay.locator.getByRole('radio', { name: typeOfSecondGoal }).check();
+	await secondInvalidateRequest;
+
+	// Verify goal and measure descriptions are persisted
+	await dotsBoard.card(titleOfFirstGoal).click();
+	await expect(dotsBoard.overlay.locator.getByLabel('Goal type')).toHaveText(typeOfFirstGoal);
+	await dotsBoard.card(titleOfSecondGoal).click();
+	await expect(dotsBoard.overlay.locator.getByLabel('Goal type')).toHaveText(typeOfSecondGoal);
+});
