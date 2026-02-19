34b6cb9ea7794568a5d63f633fa9ebac7a59c4eb
Stefan Freudenberg <stefan@knotdots.de>
2026-02-19 13:37:35 +0100

    Add test for uploading cover image

---
+++ app/tests/content-management.test.ts
@@ -0,0 +1,18 @@
+import path from 'node:path';
+import { expect, test } from './fixtures';
+
+test.use({ storageState: 'tests/.auth/admin.json' });
+
+test('stage', async ({ page, testOrganization }) => {
+	await page.goto(`/${testOrganization.guid}/all/page`);
+	await page.getByRole('checkbox', { name: 'Edit mode' }).check();
+
+	const fileChooserPromise = page.waitForEvent('filechooser');
+	await page.getByRole('button', { name: 'Add cover' }).click();
+	const fileChooser = await fileChooserPromise;
+	await fileChooser.setFiles(path.resolve(import.meta.dirname, 'cover.jpeg'));
+
+	await page.getByRole('dialog').getByRole('button', { name: 'Upload' }).click();
+	await expect(page.getByRole('dialog')).not.toBeVisible();
+	await expect(page.getByRole('img', { name: 'Cover' })).toBeVisible();
+});
