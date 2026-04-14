723a86261ed0d6164d550c1f8b602ee52c9df576
Clemens Büttner <clemens_buettner@gmx.de>
2026-04-14 16:27:24 +0200

    Replace template of FilterDropDown with InlineFilterDropDown

--- app/src/lib/components/InlineFilterDropDown.svelte
+++ app/src/lib/components/InlineFilterDropDown.svelte
@@ -5,12 +5,20 @@
 	import ChevronDown from '~icons/flowbite/chevron-down-outline';
 	import ChevronUp from '~icons/flowbite/chevron-up-outline';
 	import LightningBolt from '~icons/knotdots/lightning-bolt';
+	import FilterDisclosureOption from '$lib/components/FilterDisclosureOption.svelte';
+
+	type Option = {
+		count?: number;
+		value: string;
+		label: string;
+		subOptions?: Option[];
+	};
 
 	interface Props {
 		key: string;
 		label?: string;
 		mode: 'select' | 'apply_rule';
-		options: Array<{ count?: number; value: string; label: string }>;
+		options: Option[];
 		value: string[];
 	}
 
@@ -50,6 +58,12 @@
 			}
 		]
 	};
+
+	function hasMatchingSubOptions(option: Option) {
+		return (
+			option.subOptions?.some((sub) => (sub.count ?? 0) > 0 || value.includes(sub.value)) ?? false
+		);
+	}
 </script>
 
 <div class="dropdown" use:popperRef>
@@ -69,26 +83,12 @@
 	{#if $popover.expanded}
 		<fieldset class="dropdown-panel" use:popperContent={extraOpts} use:popover.panel>
 			<div>
-				{#each options.filter(({ count }) => count === undefined || count > 0) as option (option.value)}
-					<label>
-						<input bind:group={value} name={key} type="checkbox" value={option.value} />
-						<span class="badge badge--gray">
-							{option.label}
-							{#if option.count !== undefined}
-								<span class="counter">({option.count})</span>
-							{/if}
-						</span>
-					</label>
+				{#each options.filter((option) => option.count === undefined || option.count > 0 || hasMatchingSubOptions(option)) as option (option.value)}
+					<FilterDisclosureOption {option} bind:selected={value} />
 				{/each}
 				<p>{$_('filter.no_results')}</p>
-				{#each options.filter(({ count }) => count !== undefined && count === 0) as option (option.value)}
-					<label>
-						<input bind:group={value} name={key} type="checkbox" value={option.value} />
-						<span class="badge badge--gray">
-							{option.label}
-							<span class="counter">({option.count})</span>
-						</span>
-					</label>
+				{#each options.filter((option) => option.count !== undefined && option.count === 0 && !hasMatchingSubOptions(option)) as option (option.value)}
+					<FilterDisclosureOption {option} bind:selected={value} />
 				{/each}
 			</div>
 		</fieldset>
@@ -96,10 +96,6 @@
 </div>
 
 <style>
-	.counter {
-		color: var(--color-gray-500);
-	}
-
 	.dropdown {
 		--dropdown-button-default-background: transparent;
 		--dropdown-button-active-background: var(--color-primary-100);
@@ -113,6 +109,7 @@
 	}
 
 	.dropdown-panel {
+		max-width: min(24rem, calc(100cqw - 3rem));
 		z-index: 2;
 	}
 
