b47e931a895fe3a6f19dea24833d9f4e616a1969
Stefan Freudenberg <stefan@knotdots.de>
2026-04-16 13:34:28 +0200

    Ensure alphabetical sort takes name property into account

--- app/src/lib/server/db.ts
+++ app/src/lib/server/db.ts
@@ -676,7 +676,7 @@ function prepareWhereCondition(filters: {
 }
 
 function prepareOrderByExpression(sort: string) {
-	let order_by = sql.fragment`(c.payload->>'title')COLLATE human_sort, c.guid`;
+	let order_by = sql.fragment`(c.payload->>'title') COLLATE human_sort, (c.payload->>'name') COLLATE human_sort, c.guid`;
 	if (sort == 'modified') {
 		order_by = sql.fragment`c.valid_from DESC, c.guid`;
 	} else if (sort == 'priority') {
