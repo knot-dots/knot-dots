c09dae014ccd948569a273842c459c900c8e6e14
Niels Neumann <niels@knotdots.de>
2026-04-30 15:26:20 +0200

    Reduce separat database connections in loadApplicationContext

--- app/src/lib/server/applicationContext.ts
+++ app/src/lib/server/applicationContext.ts
@@ -17,6 +17,7 @@ import {
 	getManyOrganizationContainers,
 	setUp
 } from '$lib/server/db';
+import type { DatabaseConnection } from 'slonik';
 
 interface LoadApplicationContextParams {
 	locals: App.Locals;
@@ -31,94 +32,98 @@ export async function loadApplicationContext({
 	params,
 	url
 }: LoadApplicationContextParams) {
-	async function filterVisibleAsync<T extends AnyContainer>(promise: Promise<Array<T>>) {
-		const containers = await promise;
-		return filterVisible(containers, locals.user);
-	}
+	return await locals.pool.connect(async (connection) => {
+		const connect = <T>(fn: (connection: DatabaseConnection) => Promise<T>) => fn(connection);
 
-	const [organizations, organizationalUnits] = await Promise.all([
-		filterVisibleAsync(locals.pool.connect(getManyOrganizationContainers({}, 'alpha'))),
-		filterVisibleAsync(
-			locals.pool.connect(
-				getManyOrganizationalUnitContainers({
-					exclude: {
-						organizationalUnitType: [
-							organizationalUnitType.enum['organizational_unit_type.administrative_area']
-						]
-					}
-				})
+		async function filterVisibleAsync<T extends AnyContainer>(promise: Promise<Array<T>>) {
+			const containers = await promise;
+			return filterVisible(containers, locals.user);
+		}
+
+		const [organizations, organizationalUnits] = await Promise.all([
+			filterVisibleAsync(connect(getManyOrganizationContainers({}, 'alpha'))),
+			filterVisibleAsync(
+				connect(
+					getManyOrganizationalUnitContainers({
+						exclude: {
+							organizationalUnitType: [
+								organizationalUnitType.enum['organizational_unit_type.administrative_area']
+							]
+						}
+					})
+				)
 			)
-		)
-	]);
+		]);
 
-	let currentOrganizationalUnit: OrganizationalUnitContainer | undefined;
+		let currentOrganizationalUnit: OrganizationalUnitContainer | undefined;
 
-	if (params?.guid) {
-		try {
-			const containerFromParams = await locals.pool.connect(getContainerByGuid(params.guid));
-			if (
-				isOrganizationalUnitContainer(containerFromParams) &&
-				defineAbilityFor(locals.user).can('read', containerFromParams)
-			) {
-				currentOrganizationalUnit = containerFromParams;
-			}
-		} catch {
-			// Do nothing.
-		}
-	}
-
-	let currentOrganization: OrganizationContainer | undefined;
-
-	if (env.PUBLIC_DONT_USE_SUBDOMAINS) {
-		if (currentOrganizationalUnit) {
-			currentOrganization = organizations.find(
-				({ guid }) => guid === currentOrganizationalUnit.organization
-			);
-		} else if (params?.guid) {
-			currentOrganization = organizations.find(({ guid }) => guid === params.guid);
-		} else {
-			currentOrganization = organizations.find(({ payload }) => payload.default);
-			if (!currentOrganization) {
-				currentOrganization = (await locals.pool.connect(
-					setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
-				)) as OrganizationContainer;
+		if (params?.guid) {
+			try {
+				const containerFromParams = await connect(getContainerByGuid(params.guid));
+				if (
+					isOrganizationalUnitContainer(containerFromParams) &&
+					defineAbilityFor(locals.user).can('read', containerFromParams)
+				) {
+					currentOrganizationalUnit = containerFromParams;
+				}
+			} catch {
+				// Do nothing.
 			}
 		}
-	} else {
-		if (url.hostname === new URL(env.PUBLIC_BASE_URL ?? '').hostname) {
-			currentOrganization = organizations.find(({ payload }) => payload.default);
-			if (!currentOrganization) {
-				currentOrganization = (await locals.pool.connect(
-					setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
-				)) as OrganizationContainer;
+
+		let currentOrganization: OrganizationContainer | undefined;
+
+		if (env.PUBLIC_DONT_USE_SUBDOMAINS) {
+			if (currentOrganizationalUnit) {
+				currentOrganization = organizations.find(
+					({ guid }) => guid === currentOrganizationalUnit.organization
+				);
+			} else if (params?.guid) {
+				currentOrganization = organizations.find(({ guid }) => guid === params.guid);
+			} else {
+				currentOrganization = organizations.find(({ payload }) => payload.default);
+				if (!currentOrganization) {
+					currentOrganization = (await connect(
+						setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
+					)) as OrganizationContainer;
+				}
 			}
 		} else {
-			currentOrganization = organizations.find(({ guid }) => url.hostname.startsWith(`${guid}.`));
+			if (url.hostname === new URL(env.PUBLIC_BASE_URL ?? '').hostname) {
+				currentOrganization = organizations.find(({ payload }) => payload.default);
+				if (!currentOrganization) {
+					currentOrganization = (await connect(
+						setUp('knotdots.net', env.PUBLIC_KC_REALM ?? '')
+					)) as OrganizationContainer;
+				}
+			} else {
+				currentOrganization = organizations.find(({ guid }) => url.hostname.startsWith(`${guid}.`));
+			}
 		}
-	}
 
-	if (!currentOrganization) {
-		error(404, { message: unwrapFunctionStore(_)('error.not_found') });
-	}
+		if (!currentOrganization) {
+			error(404, { message: unwrapFunctionStore(_)('error.not_found') });
+		}
 
-	const defaultOrganizationGuid =
-		organizations.find(({ payload }) => payload.default)?.guid ?? currentOrganization.guid;
+		const defaultOrganizationGuid =
+			organizations.find(({ payload }) => payload.default)?.guid ?? currentOrganization.guid;
 
-	const categoryContext = createFeatureDecisions(locals.features).useCustomCategories()
-		? await loadCategoryContext({
-				connect: locals.pool.connect,
-				scope: [currentOrganization.guid, defaultOrganizationGuid],
-				user: locals.user
-			})
-		: null;
+		const categoryContext = createFeatureDecisions(locals.features).useCustomCategories()
+			? await loadCategoryContext({
+					connect,
+					scope: [currentOrganization.guid, defaultOrganizationGuid],
+					user: locals.user
+				})
+			: null;
 
-	return {
-		categoryContext,
-		currentOrganization,
-		currentOrganizationalUnit,
-		defaultOrganizationGuid,
-		features: locals.features,
-		organizations,
-		organizationalUnits
-	};
+		return {
+			categoryContext,
+			currentOrganization,
+			currentOrganizationalUnit,
+			defaultOrganizationGuid,
+			features: locals.features,
+			organizations,
+			organizationalUnits
+		};
+	});
 }
