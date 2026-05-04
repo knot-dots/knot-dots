82e2651d29a5d4ba504fcd4ebc92e432748cf9f1
Stefan Freudenberg <stefan@knotdots.de>
2026-05-04 11:53:52 +0200

    Add another organization to condition

--- migrate/sql/20260430065454_merge_categories.up.sql
+++ migrate/sql/20260430065454_merge_categories.up.sql
@@ -12,7 +12,7 @@ SET payload = jsonb_set(
     )),
     true
 )
-WHERE organization IN ('05b69915-1dba-4094-860d-ed290f119a41', '0b4528ed-433d-4e29-aabc-6dedd44992c6', '0bddac59-17e7-48d7-9b66-f53ff5ce5521', '3aedc661-1c69-46ac-8a39-f143615eb956', '59a5bdb6-82da-48d6-8a32-4d854b4a8fcc', '743487a1-e49e-4526-bebd-2b3fe9e838e5', 'dede21cf-926e-4b9a-990f-b38dd0f7304b', 'e6553ad1-93ab-4d14-9ba2-d7a86826c71e')
+WHERE organization IN ('05b69915-1dba-4094-860d-ed290f119a41', '0b4528ed-433d-4e29-aabc-6dedd44992c6', '0bddac59-17e7-48d7-9b66-f53ff5ce5521', '3aedc661-1c69-46ac-8a39-f143615eb956', '59a5bdb6-82da-48d6-8a32-4d854b4a8fcc', '743487a1-e49e-4526-bebd-2b3fe9e838e5', 'dede21cf-926e-4b9a-990f-b38dd0f7304b', 'e6553ad1-93ab-4d14-9ba2-d7a86826c71e', 'dd56df72-074a-43c1-8092-88c1c6a6e2ad')
     AND payload ? 'category'
     AND (payload ? 'sdg'
     OR payload ? 'topic'
