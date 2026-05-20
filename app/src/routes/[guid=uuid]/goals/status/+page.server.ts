662b7e3b08b7d801ab936b0681f104ee24a43e44
Niels Neumann <niels@knotdots.de>
2026-05-20 15:49:57 +0200

    Implement column pagination for goals board and add query utility function

--- app/src/routes/[guid=uuid]/goals/status/+page.server.ts
+++ app/src/routes/[guid=uuid]/goals/status/+page.server.ts
@@ -1,3 +1,13 @@
-import { loadPage } from '$lib/load/goals';
+import { loadColumnBoardPage } from '$lib/load/columnBoard';
+import { type GoalContainer, goalStatus, type GoalStatus, payloadTypes } from '$lib/models';
+import { DEFAULT_PAGE_SIZE } from '$lib/pagination';
+import { createGoalStatusQuery, DEFAULT_RELATION_TYPES } from './query';
+import type { PageServerLoad } from './$types';
 
-export const load = loadPage(10_000);
+export const load = loadColumnBoardPage<GoalContainer, GoalStatus>({
+	createQuery: createGoalStatusQuery,
+	defaultRelationTypes: DEFAULT_RELATION_TYPES,
+	getColumnIds: () => goalStatus.options,
+	limit: DEFAULT_PAGE_SIZE,
+	payloadTypes: [payloadTypes.enum.goal]
+}) satisfies PageServerLoad;
