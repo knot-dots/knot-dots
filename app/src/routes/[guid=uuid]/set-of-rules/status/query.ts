16209013248291236f1c48647553058eaa74f71b
Niels Neumann <niels@knotdots.de>
2026-05-21 17:13:36 +0200

    Implement pagination for set-of-rules status board

---
+++ app/src/routes/[guid=uuid]/set-of-rules/status/query.ts
@@ -0,0 +1,30 @@
+import { type ProgramStatus, payloadTypes, predicates } from '$lib/models';
+
+export const DEFAULT_RELATION_TYPES = [
+	predicates.enum['is-consistent-with'],
+	predicates.enum['is-equivalent-to'],
+	predicates.enum['is-inconsistent-with'],
+	predicates.enum['is-superordinate-of']
+];
+
+export function createSetOfRulesStatusQuery(url: URL, status?: ProgramStatus) {
+	const query = new URLSearchParams(url.searchParams);
+	query.delete('payloadType');
+	query.delete('type');
+	query.delete('programStatus');
+	query.delete('programType');
+	query.append('programType', 'program_type.set_of_rules');
+	query.append('type', payloadTypes.enum.program);
+
+	if (status) {
+		query.append('programStatus', status);
+	}
+
+	if (url.searchParams.has('related-to') && !url.searchParams.has('relationType')) {
+		for (const relationType of DEFAULT_RELATION_TYPES) {
+			query.append('relationType', relationType);
+		}
+	}
+
+	return query;
+}
