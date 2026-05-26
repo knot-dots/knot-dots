1c4710bd81ba48698ba73ae16f9c90d4c9e698be
Niels Neumann <niels@knotdots.de>
2026-05-26 17:31:07 +0200

    Implement pagination for goals level board

--- app/src/routes/[guid=uuid]/goals/level/+page.server.ts
+++ app/src/routes/[guid=uuid]/goals/level/+page.server.ts
@@ -1,3 +1,15 @@
-import { loadPage } from '$lib/load/goals';
+import { loadColumnBoardPage } from '$lib/load/columnBoard';
+import { type GoalContainer, payloadTypes } from '$lib/models';
+import { DEFAULT_PAGE_SIZE } from '$lib/pagination';
+import { createGoalLevelQuery, DEFAULT_RELATION_TYPES } from './query';
+import type { PageServerLoad } from './$types';
 
-export const load = loadPage(10_000);
+const HIERARCHY_LEVELS = ['1', '2', '3', '4', '5', '6'] as const;
+
+export const load = loadColumnBoardPage<GoalContainer, string>({
+	createQuery: createGoalLevelQuery,
+	defaultRelationTypes: DEFAULT_RELATION_TYPES,
+	getColumnIds: () => HIERARCHY_LEVELS,
+	limit: DEFAULT_PAGE_SIZE,
+	payloadTypes: [payloadTypes.enum.goal]
+}) satisfies PageServerLoad;
