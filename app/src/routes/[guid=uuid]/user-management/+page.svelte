a5e7c0c3893a11b4f304c30a6b271097f2527615
Niels Neumann <niels@knotdots.de>
2026-06-17 11:04:57 +0200

    Rename variable in load function to avoid overwrite hazard

--- app/src/routes/[guid=uuid]/user-management/+page.svelte
+++ app/src/routes/[guid=uuid]/user-management/+page.svelte
@@ -71,7 +71,7 @@
 
 	const organizationColumns = $derived([
 		{ container: data.container },
-		...data.organizationalUnits.map((container) => ({
+		...data.managedOrganizationalUnits.map((container) => ({
 			container
 		}))
 	]);
