ea12399b5e44c7abfa3f9960886a4356f83d4ee9
Stefan Freudenberg <stefan@knotdots.de>
2026-07-17 14:18:55 +0200

    Ensure default organization is handled correctly in rewrite map

--- app/src/routes/rewrite-map/+server.ts
+++ app/src/routes/rewrite-map/+server.ts
@@ -16,19 +16,23 @@ export const GET: RequestHandler = async ({ locals }) => {
 	const rewriteMap: RewriteMap = {};
 
 	for (const organization of organizations) {
-		const hostname = `${organization.guid}.${baseURL.hostname}`;
+		const hostname = organization.payload.default
+			? baseURL.hostname
+			: `${organization.guid}.${baseURL.hostname}`;
 
-		rewriteMap[hostname] = Object.fromEntries(
-			organizationalUnits
-				.filter((ou) => ou.organization == organization.guid && ou.payload.slug)
-				.map(({ guid, payload }) => [payload.slug as string, guid])
-		);
+		rewriteMap[hostname] = {
+			...(organization.payload.slug
+				? { [organization.payload.slug]: organization.guid }
+				: undefined),
+			...Object.fromEntries(
+				organizationalUnits
+					.filter((ou) => ou.organization == organization.guid && ou.payload.slug)
+					.map(({ guid, payload }) => [payload.slug as string, guid])
+			)
+		};
 
-		if (organization.payload.slug) {
-			rewriteMap[`${organization.payload.slug}.${baseURL.hostname}`] = {
-				...rewriteMap[hostname],
-				[organization.payload.slug]: organization.guid
-			};
+		if (organization.payload.slug && !organization.payload.default) {
+			rewriteMap[`${organization.payload.slug}.${baseURL.hostname}`] = rewriteMap[hostname];
 		}
 
 		if (organization.payload.customDomain) {
