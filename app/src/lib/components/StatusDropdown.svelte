fafcc7ac7a4e2f72b96d7bab8882243f68fb8d54
Stefan Freudenberg <stefan@knotdots.de>
2026-08-05 18:25:32 +0200

    Adapt badge-style drop-downs to new design

--- app/src/lib/components/StatusDropdown.svelte
+++ app/src/lib/components/StatusDropdown.svelte
@@ -94,6 +94,10 @@
 {/if}
 
 <style>
+	.dropdown-button {
+		--dropdown-button-border-radius: 8px;
+	}
+
 	.dropdown-button.dropdown-button--select,
 	.value {
 		padding-left: var(--dropdown-button-padding-y);
